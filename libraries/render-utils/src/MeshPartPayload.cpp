3aab937b0b09a74ca1164f18e9788345dc8086f8
HifiExperiments <thingsandstuffblog@gmail.com>
2021-02-02 22:17:15 -0800

    fix model velocity vector

--- libraries/render-utils/src/MeshPartPayload.cpp
+++ libraries/render-utils/src/MeshPartPayload.cpp
@@ -194,7 +194,9 @@ void ModelMeshPartPayload::bindTransform(gpu::Batch& batch, const Transform& tra
         batch.setUniformBuffer(graphics::slot::buffer::Skinning, _clusterBuffer);
     }
     batch.setModelTransform(transform, _previousModelTransform);
-    _previousModelTransform = _drawTransform;
+    if (renderMode == Args::RenderMode::DEFAULT_RENDER_MODE || renderMode == Args::RenderMode::MIRROR_RENDER_MODE) {
+        _prevRenderTransform = _drawTransform;
+    }
 }
 
 void ModelMeshPartPayload::drawCall(gpu::Batch& batch) const {
