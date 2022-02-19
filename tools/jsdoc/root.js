e943636fa846c825b06756e451091e2fa978950c
Julian Groß <firedefender1@googlemail.com>
2022-02-19 17:59:44 +0100

    Update jsdoc

--- tools/jsdoc/root.js
+++ tools/jsdoc/root.js
@@ -3,6 +3,7 @@
 //
 //  Copyright 2016 High Fidelity, Inc.
 //  Copyright 2021 Vircaida contributors.
+//  Copyright 2022 Overte e.V.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
