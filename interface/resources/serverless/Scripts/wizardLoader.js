14f80f92ef303b88c76d14eb223d9d761b1d6ae9
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2022-02-27 13:36:15 -0500

    Change the url to have the files reachable.
    Change the url to have the files reachable.

--- interface/resources/serverless/Scripts/wizardLoader.js
+++ interface/resources/serverless/Scripts/wizardLoader.js
@@ -13,7 +13,7 @@
 //
 
 (function() {
-    var CONFIG_WIZARD_URL = "qrc:///serverless/Scripts/wizard.html?v=" + Math.floor(Math.random() * 65000);
+    var CONFIG_WIZARD_URL = "file:///~/serverless/Scripts/wizard.html?v=" + Math.floor(Math.random() * 65000);
 
     var loaderEntityID;
     var configWizardEntityID;
