6c143019636db2a1867d9d319c5b4404cbe63da8
Julian Groß <firedefender1@googlemail.com>
2022-03-13 03:11:46 +0100

    upload_to_publish_server.py aktualisiert

--- tools/ci-scripts/upload_to_publish_server.py
+++ tools/ci-scripts/upload_to_publish_server.py
@@ -41,7 +41,8 @@ for archiveFile in files:
     file_contents.append(file_data)
     file.close()
 
-conn = http.client.HTTPSConnection("build-uploader.vircadia.com")
+# Server running https://github.com/thoys/gha-artifact-uploader
+conn = http.client.HTTPSConnection("172.104.202.151")
 
 context = json.loads(os.environ['GITHUB_CONTEXT'])
 
