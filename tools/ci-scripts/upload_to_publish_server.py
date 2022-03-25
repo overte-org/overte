c63caeb793c60059338618d20f0ce2cf14c4dbd6
Julian Groß <firedefender1@googlemail.com>
2022-03-25 03:02:36 +0100

    pr_build.yml und upload_to_publish_server.py aktualisiert

--- tools/ci-scripts/upload_to_publish_server.py
+++ tools/ci-scripts/upload_to_publish_server.py
@@ -41,7 +41,7 @@ for archiveFile in files:
     file_contents.append(file_data)
     file.close()
 
-conn = http.client.HTTPSConnection("build-uploader.vircadia.com")
+conn = http.client.HTTPSConnection("172.104.202.151:3000")
 
 context = json.loads(os.environ['GITHUB_CONTEXT'])
 
