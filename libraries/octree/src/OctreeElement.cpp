0b7934a8b342037d66a2124a0f06aebe49c5424d
Dale Glass <dale@daleglass.net>
2022-06-10 00:47:47 +0200

    Remove unused variable

--- libraries/octree/src/OctreeElement.cpp
+++ libraries/octree/src/OctreeElement.cpp
@@ -442,15 +442,12 @@ void OctreeElement::printDebugDetails(const char* label) const {
         }
     }
 
-    QString resultString;
     qCDebug(octree).noquote() << label
                               << QString(" - Voxel at corner=(%1,%2,%3)").arg((double)_cube.getCorner().x, (double)_cube.getCorner().y, (double)_cube.getCorner().z)
                               << "size=" << (double)_cube.getScale()
                               << " isLeaf=" << debug::valueOf(isLeaf())
                               << " isDirty=" << debug::valueOf(isDirty())
                               << " shouldRender=" << debug::valueOf(getShouldRender());
-
-    qCDebug(octree).nospace() << resultString;
 }
 
 float OctreeElement::getEnclosingRadius() const {
