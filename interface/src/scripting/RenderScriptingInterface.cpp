71e214a0338b670a1f59a8666a88e449932b8bb2
ksuprynowicz <ksuprynowicz@post.pl>
2022-08-14 22:05:50 +0200

    Attempt to debug broken enums in scripting

--- interface/src/scripting/RenderScriptingInterface.cpp
+++ interface/src/scripting/RenderScriptingInterface.cpp
@@ -23,6 +23,7 @@ RenderScriptingInterface::RenderScriptingInterface() {
     std::call_once(registry_flag, [] {
         qmlRegisterType<RenderScriptingInterface>("RenderEnums", 1, 0, "RenderEnums");
         qRegisterMetaType<RenderScriptingInterface::RenderMethod>("RenderScriptingInterface::RenderMethod");
+        qDebug() << "qRegisterMetaType<RenderScriptingInterface::RenderMethod>(\"RenderScriptingInterface::RenderMethod\")";
     });
 }
 
