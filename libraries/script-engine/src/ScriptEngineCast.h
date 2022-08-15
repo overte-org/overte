13c755903fac494df0ded0879420c101499813b8
ksuprynowicz <ksuprynowicz@post.pl>
2022-08-15 21:00:45 +0200

    Proper handling of enum classes for scripting

--- libraries/script-engine/src/ScriptEngineCast.h
+++ libraries/script-engine/src/ScriptEngineCast.h
@@ -4,6 +4,7 @@
 //
 //  Created by Heather Anderson on 5/9/2021.
 //  Copyright 2021 Vircadia contributors.
+//  Copyright 2022 Overte e.V.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
@@ -18,6 +19,8 @@
 // Object conversion helpers (copied from QScriptEngine)
 
 #include <QtCore/QMetaType>
+#include <QtCore/QMetaEnum>
+#include <QtCore/QDebug>
 
 #include "ScriptEngine.h"
 #include "ScriptValue.h"
@@ -66,10 +69,15 @@ inline QVariant scriptvalue_cast<QVariant>(const ScriptValue& value) {
 template <typename T>
 int scriptRegisterMetaType(ScriptEngine* eng,
                            ScriptValue (*toScriptValue)(ScriptEngine*, const T& t),
-                           bool (*fromScriptValue)(const ScriptValue&, T& t),
+                           bool (*fromScriptValue)(const ScriptValue&, T& t), const char* name = "",
                            T* = 0)
 {
-    const int id = qRegisterMetaType<T>();  // make sure it's registered
+    int id;
+    if (strlen(name) > 0) { // make sure it's registered
+        id = qRegisterMetaType<T>(name);
+    } else {
+        id = qRegisterMetaType<T>();
+    }
     eng->registerCustomType(id, reinterpret_cast<ScriptEngine::MarshalFunction>(toScriptValue),
                             reinterpret_cast<ScriptEngine::DemarshalFunction>(fromScriptValue));
     return id;
@@ -96,6 +104,40 @@ bool scriptValueToSequence(const ScriptValue& value, Container& cont) {
         cont.push_back(scriptvalue_cast<typename Container::value_type>(item));
     }
     return true;
+}
+
+template <class T>
+ScriptValue scriptValueFromEnumClass(ScriptEngine* eng, const T& enumValue) {
+    ScriptValue a = eng->newValue(static_cast<int>(enumValue));
+    return a;
+}
+
+template <class T>
+bool scriptValueToEnumClass(const ScriptValue& value, T& enumValue) {
+    if(!value.isNumber()){
+        qDebug() << "ScriptValue \"" << value.toQObject()->metaObject()->className() << "\" is not a number";
+        return false;
+    }
+    QMetaEnum metaEnum = QMetaEnum::fromType<T>();
+    if (!metaEnum.isValid()) {
+        qDebug() << "Invalid QMetaEnum";
+        return false;
+    }
+    bool isValid = false;
+    int enumInteger = static_cast<int>(value.toInteger());
+    for(int i = 0; i < metaEnum.keyCount(); i++){
+        if (metaEnum.value(i) == enumInteger) {
+            isValid = true;
+            break;
+        }
+    }
+    if (isValid) {
+        enumValue = static_cast<T>(enumInteger);
+        return true;
+    } else {
+        qDebug() << "ScriptValue has invalid value " << value.toInteger() << " for enum" << metaEnum.name();
+        return false;
+    }
 }
 
 template <typename T>
