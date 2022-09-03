84c47197754c55aa2fc3058e6bedce11fa285bc9
ksuprynowicz <ksuprynowicz@post.pl>
2022-09-03 14:37:23 +0200

    Initial cmake files for V8

---
+++ cmake/macros/TargetV8.cmake
@@ -0,0 +1,14 @@
+#
+#  Copyright 2022 Overte e.V.
+#  Created by dr Karol Suprynowicz on 2022/09/03
+#
+#  Distributed under the Apache License, Version 2.0.
+#  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+#
+macro(TARGET_V8)
+
+find_package(V8 REQUIRED)
+target_include_directories(${TARGET_NAME} PUBLIC ${V8_INCLUDE_DIRS})
+target_link_libraries(${TARGET_NAME} ${V8_LIBRARIES})
+
+endmacro()
