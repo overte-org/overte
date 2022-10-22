b5d9a4dbba92600613aaf68e4a4c4093d846387e
Dale Glass <dale@daleglass.net>
2022-10-23 01:02:16 +0200

    Make test class a friend

--- libraries/shared/src/SettingManager.h
+++ libraries/shared/src/SettingManager.h
@@ -20,6 +20,12 @@
 #include "DependencyManager.h"
 #include "shared/ReadWriteLockable.h"
 
+
+// This is for the testing system.
+class SettingsTests;
+
+
+
 namespace Setting {
     class Interface;
 
@@ -67,6 +73,8 @@ namespace Setting {
         QHash<QString, QVariant> _pendingChanges;
 
         friend class Interface;
+        friend class SettingsTests;
+
         friend void cleanupSettingsSaveThread();
         friend void setupSettingsSaveThread();
 
