51d16c2ecf9af33d21dd7f86511ca32db6753c9d
Dale Glass <dale@daleglass.net>
2022-10-23 16:05:22 +0200

    Fix copyright header

--- tests/shared/src/SettingsTests.cpp
+++ tests/shared/src/SettingsTests.cpp
@@ -1,6 +1,6 @@
 //
-//  Created by Bradley Austin Davis on 2017/11/08
-//  Copyright 2013-2017 High Fidelity, Inc.
+//  Created by Dale Glass on 2022/10/22
+//  Copyright 2022 Overte e.V.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
