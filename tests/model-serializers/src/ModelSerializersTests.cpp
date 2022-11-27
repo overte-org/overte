38854a8dda4d57523ca9557e4c9a12d50195cdb6
Dale Glass <dale@daleglass.net>
2022-11-27 13:47:58 +0100

    Add the usage of the glTF-Sample-Models test models.

--- tests/model-serializers/src/ModelSerializersTests.cpp
+++ tests/model-serializers/src/ModelSerializersTests.cpp
@@ -9,6 +9,14 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
 
+// This test checks a large amount of files. To debug more comfortably and avoid going through
+// a lot of uninteresting data, QTest allows us to narrow down what gets run with command line
+// arguments, like this:
+//
+//     ./model-serializers-ModelSerializersTests loadGLTF:gltf2.0-RecursiveSkeletons.glb
+//
+// This will run only the loadGLTF test, and only on the gltf2.0-RecursiveSkeletons.glb file.
+
 #include "ModelSerializersTests.h"
 #include "GLTFSerializer.h"
 #include "FBXSerializer.h"
@@ -29,7 +37,7 @@
 #include <QNetworkReply>
 #include <QByteArray>
 #include <QDebug>
-
+#include <QDirIterator>
 
 QTEST_MAIN(ModelSerializersTests)
 
@@ -52,21 +60,45 @@ void ModelSerializersTests::initTestCase() {
 }
 
 void ModelSerializersTests::loadGLTF_data() {
+    // We feed a large amount of files into the test. Some we expect to fail, some we expect to load with issues. The
+    // added columns below indicate our expectations for each file.
+
+
     QTest::addColumn<QString>("filename");
+    QTest::addColumn<bool>("expectParseFail");
     QTest::addColumn<bool>("expectWarnings");
     QTest::addColumn<bool>("expectErrors");
 
-    QTest::newRow("ready-player-me-good1")   << "models/src/DragonAvatar1.glb.gz" << false << false;
-    QTest::newRow("ready-player-me-good2")   << "models/src/UkraineFranny.glb.gz" << false << false;
-    QTest::newRow("ready-player-me-good3")   << "models/src/Franny.glb.gz" << false << false;
-    QTest::newRow("ready-player-me-good4")   << "models/src/womanInTShirt.glb.gz" << false << false;
-    QTest::newRow("ready-player-me-good5")   << "models/src/female-avatar-with-swords.glb.gz" << false << false;
+    QTest::newRow("ready-player-me-good1")   << "models/src/DragonAvatar1.glb.gz"              << false << false << false;
+    QTest::newRow("ready-player-me-good2")   << "models/src/UkraineFranny.glb.gz"              << false << false << false;
+    QTest::newRow("ready-player-me-good3")   << "models/src/Franny.glb.gz"                     << false << false << false;
+    QTest::newRow("ready-player-me-good4")   << "models/src/womanInTShirt.glb.gz"              << false << false << false;
+    QTest::newRow("ready-player-me-good5")   << "models/src/female-avatar-with-swords.glb.gz"  << false << false << false;
     QTest::newRow("ready-player-me-broken1") << "models/src/broken-2022-11-27.glb.gz" << false << true;
+
+
+    // We can't parse GLTF 1.0 at present, and probably not ever. We're expecting all these to fail.
+    QDirIterator it("models/src/gltf_samples/1.0", QStringList() << "*.glb", QDir::Files, QDirIterator::Subdirectories);
+    while(it.hasNext()) {
+        QString filename = it.next();
+        QFileInfo fi(filename);
+        QString testname = "gltf1.0-" + fi.fileName();
+        QTest::newRow(testname.toUtf8().data()) << filename << true << false << false;
+    }
+
+    QDirIterator it2("models/src/gltf_samples/2.0", QStringList() << "*.glb", QDir::Files, QDirIterator::Subdirectories);
+    while(it2.hasNext()) {
+        QString filename = it2.next();
+        QFileInfo fi(filename);
+        QString testname = "gltf2.0-" + fi.fileName();
+        QTest::newRow(testname.toUtf8().data()) << filename << false << false << false;
+    }
 
 }
 
 void ModelSerializersTests::loadGLTF() {
     QFETCH(QString, filename);
+    QFETCH(bool, expectParseFail);
     QFETCH(bool, expectWarnings);
     QFETCH(bool, expectErrors);
 
@@ -110,7 +142,13 @@ void ModelSerializersTests::loadGLTF() {
 
 
     hfm::Model::Pointer model = loader.load(uncompressedData, serializerMapping, url, webMediaType);
-    QVERIFY(model);
+    QVERIFY(expectParseFail == !model);
+
+    if (!model) {
+        // We expected this parse to fail, so nothing more to do here.
+        return;
+    }
+
     QVERIFY(!model->meshes.empty());
     QVERIFY(!model->joints.empty());
 
