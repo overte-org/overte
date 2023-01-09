c49766c9783aa2d3f52cc0780fef54652d4ad401
ksuprynowicz <ksuprynowicz@post.pl>
2023-01-09 20:24:49 +0100

    Added debug function to list object members

--- libraries/script-engine/src/ScriptEngine.h
+++ libraries/script-engine/src/ScriptEngine.h
@@ -128,7 +128,7 @@ public:
     virtual void requestCollectGarbage() = 0;
     virtual void compileTest() = 0;
     virtual QString scriptValueDebugDetails(const ScriptValue &value) = 0;
-
+    virtual QString scriptValueDebugListMembers(const ScriptValue &value) = 0;
 public:
     // helper to detect and log warnings when other code invokes QScriptEngine/BaseScriptEngine in thread-unsafe ways
     bool IS_THREADSAFE_INVOCATION(const QString& method);
