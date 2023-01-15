1bf90544dab5009e909fc03c3bd04c4dd4e0ed71
ksuprynowicz <ksuprynowicz@post.pl>
2023-01-15 15:58:21 +0100

    Fixed adding properties to methods

--- tests/script-engine/src/tests/004_require.js
+++ tests/script-engine/src/tests/004_require.js
@@ -1,3 +1,9 @@
+print("Script.require: " + JSON.stringify(Script.require));
+print("Script.require.cache: " + JSON.stringify(Script.require.cache));
+print(JSON.stringify("Script.require.test_prop before defining: " + Script.require.test_prop));
+Script.require.test_prop = "test property";
+print(JSON.stringify("Script.require.test_prop after defining: " + Script.require.test_prop));
 print("Before require");
-Script.require("001_test_print.js");
+// TODO: find correct local path for the test module
+Script.require("http://oaktown.pl/scripts/001_test_print.js");
 print("After require");
