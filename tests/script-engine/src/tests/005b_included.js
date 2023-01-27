1f9df59dd9a96a7c57b51510f1966c24b3faa748
ksuprynowicz <ksuprynowicz@post.pl>
2023-01-27 19:06:19 +0100

    V8 threading safetyu improvements

--- tests/script-engine/src/tests/005b_included.js
+++ tests/script-engine/src/tests/005b_included.js
@@ -5,4 +5,19 @@ function functionInInclude() {
     functionInMainFile();
 }
 
+var TestObject = function(shouldUseEditTabletApp) {
+    var requireTest = Script.require("http://oaktown.pl/scripts/004b_require_module.js");
+
+    var onEvent = function(data) {
+        that.thatFunction();
+    }
+    var that = {};
+    that.thatFunction = function() {
+        functionInMainFile();
+    }
+    Script.update.connect(onEvent);
+}
+
 functionInMainFile();
+
+//Script.setInterval(functionInInclude, 1000);
