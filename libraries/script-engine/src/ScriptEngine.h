e35aa8046dffe34e7d6205bde6f2818ad0b10147
Dale Glass <dale@daleglass.net>
2023-02-23 23:45:36 +0100

    Add more Doxygen docs
    Doesn't work right in ScriptManager, looks like a conflict with JSDoc

--- libraries/script-engine/src/ScriptEngine.h
+++ libraries/script-engine/src/ScriptEngine.h
@@ -5,6 +5,7 @@
 //  Created by Brad Hefta-Gaub on 12/14/13.
 //  Copyright 2013 High Fidelity, Inc.
 //  Copyright 2020 Vircadia contributors.
+//  Copyright 2023 Overte e.V.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
@@ -44,48 +45,126 @@ template <typename T>
 inline T scriptvalue_cast(const ScriptValue& value);
 
 
-/// [ScriptInterface] Provides an engine-independent interface for QScriptEngine
+/**
+ * @brief Provides an engine-independent interface for a scripting engine
+ *
+ * Each script engine is strictly single threaded.
+ *
+ * This class only provides an interface to the underlying scripting engine, and doesn't
+ * provide the full environment needed to execute scripts.
+ *
+ * To execute scripts that have access to the API, use ScriptManager.
+ */
 class ScriptEngine {
 public:
     typedef ScriptValue (*FunctionSignature)(ScriptContext*, ScriptEngine*);
     typedef ScriptValue (*MarshalFunction)(ScriptEngine*, const void*);
     typedef bool (*DemarshalFunction)(const ScriptValue&, QVariant &dest);
 
+    /**
+     * @brief Who owns a given object
+     *
+     */
     enum ValueOwnership {
-        QtOwnership = 0,
-        ScriptOwnership = 1,
-        AutoOwnership = 2,
+        QtOwnership = 0, /** Object is managed by Qt */
+        ScriptOwnership = 1, /** Object is managed by the script */
+        AutoOwnership = 2, /** Ownership is determined automatically */
     };
 
+    /**
+     * @brief Which part of an object is exposed to the script
+     *
+     */
     enum QObjectWrapOption {
-        //ExcludeChildObjects	= 0x0001,	// The script object will not expose child objects as properties.
-        ExcludeSuperClassMethods = 0x0002,	// The script object will not expose signals and slots inherited from the superclass.
-        ExcludeSuperClassProperties	= 0x0004,	// The script object will not expose properties inherited from the superclass.
+        //ExcludeChildObjects	= 0x0001,	/** The script object will not expose child objects as properties. */
+        ExcludeSuperClassMethods = 0x0002,	/** The script object will not expose signals and slots inherited from the superclass. */
+        ExcludeSuperClassProperties	= 0x0004,	/** The script object will not expose properties inherited from the superclass. */
         ExcludeSuperClassContents = ExcludeSuperClassMethods | ExcludeSuperClassProperties,
-        //ExcludeDeleteLater = 0x0010,	// The script object will not expose the QObject::deleteLater() slot.
-        ExcludeSlots = 0x0020,	// The script object will not expose the QObject's slots.
-        AutoCreateDynamicProperties = 0x0100,	// Properties that don't already exist in the QObject will be created as dynamic properties of that object, rather than as properties of the script object.
-        PreferExistingWrapperObject = 0x0200,	// If a wrapper object with the requested configuration already exists, return that object.
-        SkipMethodsInEnumeration = 0x0008,	// Don't include methods (signals and slots) when enumerating the object's properties.
+        //ExcludeDeleteLater = 0x0010,	/** The script object will not expose the QObject::deleteLater() slot. */
+        ExcludeSlots = 0x0020,	/** The script object will not expose the QObject's slots. */
+        AutoCreateDynamicProperties = 0x0100,	/** Properties that don't already exist in the QObject will be created as dynamic properties of that object, rather than as properties of the script object. */
+        PreferExistingWrapperObject = 0x0200,	/** If a wrapper object with the requested configuration already exists, return that object. */
+        SkipMethodsInEnumeration = 0x0008,	/** Don't include methods (signals and slots) when enumerating the object's properties. */
     };
     Q_DECLARE_FLAGS(QObjectWrapOptions, QObjectWrapOption);
 
 public:
+    /**
+     * @brief Stops the currently running script
+     *
+     */
     virtual void abortEvaluation() = 0;
+
+    /**
+     * @brief Clears uncaughtException and related
+     *
+     */
     virtual void clearExceptions() = 0;
+
     virtual ScriptValue cloneUncaughtException(const QString& detail = QString()) = 0;
+
+    /**
+     * @brief Context of the currently running script
+     *
+     * This allows getting a backtrace, the local variables of the currently running function, etc.
+     *
+     * @return ScriptContext*
+     */
     virtual ScriptContext* currentContext() const = 0;
+
+    /**
+     * @brief Runs a script
+     *
+     * This may be called several times during the lifetime of a scripting engine, with the
+     * side effects accumulating.
+     *
+     * @param program Code to run
+     * @param fileName Name of the script, for informational purposes
+     * @return ScriptValue Return value of the script when it finishes running.
+     */
     virtual ScriptValue evaluate(const QString& program, const QString& fileName = QString()) = 0;
+
+    /**
+     * @brief Evaluates a pre-compiled program
+     *
+     * @param program Program to evaluaate
+     * @return ScriptValue
+     */
     virtual ScriptValue evaluate(const ScriptProgramPointer &program) = 0;
+
+
+    /**
+     * @brief Evaluate a script in a separate environment
+     *
+     * Used for evaluating included scripts
+     *
+     * @param locals Local variables available to the script
+     * @param program Code to run
+     * @return ScriptValue
+     */
     virtual ScriptValue evaluateInClosure(const ScriptValue& locals, const ScriptProgramPointer& program) = 0;
+
+    /**
+     * @brief Global namespace, containing all the public APIs
+     *
+     * @return ScriptValue
+     */
     virtual ScriptValue globalObject() {
         Q_ASSERT(false);
         return ScriptValue();
     }
     virtual bool hasUncaughtException() const = 0;
+
+    /**
+     * @brief Whether a script is currently being evaluated
+     *
+     * @return true A script is currently being evaluated
+     * @return false No script is being evaluated
+     */
     virtual bool isEvaluating() const = 0;
     //virtual ScriptValue lintScript(const QString& sourceCode, const QString& fileName, const int lineNumber = 1) = 0;
-    virtual ScriptValue checkScriptSyntax(ScriptProgramPointer program) = 0;    
+
+    virtual ScriptValue checkScriptSyntax(ScriptProgramPointer program) = 0;
     virtual ScriptValue makeError(const ScriptValue& other = ScriptValue(), const QString& type = "Error") = 0;
     virtual ScriptManager* manager() const = 0;
     virtual bool maybeEmitUncaughtException(const QString& debugHint = QString()) = 0;
@@ -107,6 +186,15 @@ public:
     virtual ScriptValue newValue(const char* value) = 0;
     virtual ScriptValue newVariant(const QVariant& value) = 0;
     virtual ScriptValue nullValue() = 0;
+
+
+    /**
+     * @brief Causes an exception to be raised in the currently executing script
+     *
+     * @param exception
+     * @return true
+     * @return false
+     */
     virtual bool raiseException(const ScriptValue& exception) = 0;
     virtual void registerEnum(const QString& enumName, QMetaEnum newEnum) = 0;
     virtual void registerFunction(const QString& name, FunctionSignature fun, int numArguments = -1) = 0;
