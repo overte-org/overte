500ca0fd302104d84f0263c00ad8a5ef0cb56db9
Dale Glass <dale@daleglass.net>
2023-03-10 18:04:17 +0100

    Remove old tests, doesn't build on Windows

--- tests/script-engine/src/networked_tests/004b_require_module.js
+++
@@ -1,9 +0,0 @@
-print("Print inside module script");
-module.exports = {
-    moduleFunction1: function () {
-        print("moduleFunction1 works");
-    },
-    moduleFunction2: function () {
-        print("moduleFunction2 works");
-    }
-};
