8b9cd84df7ea6ff460f9c79f4d648c9de8d88945
ksuprynowicz <ksuprynowicz@post.pl>
2023-04-01 19:51:21 +0200

    vec3ToScriptValue optimizations

--- libraries/script-engine/src/v8/FastScriptValueUtils.h
+++ libraries/script-engine/src/v8/FastScriptValueUtils.h
@@ -9,7 +9,9 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-// Contains V8-specific implementations of th
+// Contains V8-specific implementations of the function converting Overte datatypes to and from script values.
+// These are used instead of generic implementations if CONVERSIONS_OPTIMIZED_FOR_V8 is defined.
+// V8-specific implementations can make script engine several times faster.
 
 #ifndef overte_FastScriptValueUtils_h
 #define overte_FastScriptValueUtils_h
@@ -19,8 +21,12 @@
 
 #include "../ScriptValue.h"
 
-ScriptValue vec3ToScriptValueFast(ScriptEngine* engine, const glm::vec3& vec3);
+#define CONVERSIONS_OPTIMIZED_FOR_V8
 
-bool vec3FromScriptValueFast(const ScriptValue& object, glm::vec3& vec3);
+#ifdef CONVERSIONS_OPTIMIZED_FOR_V8
+ScriptValue vec3ToScriptValue(ScriptEngine* engine, const glm::vec3& vec3);
+
+bool vec3FromScriptValue(const ScriptValue& object, glm::vec3& vec3);
+#endif
 
 #endif  // overte_FastScriptValueUtils_h
