328ae6c7a5490f6862db4b06921c669967b15f1c
ksuprynowicz <ksuprynowicz@post.pl>
2023-04-27 23:39:53 +0200

    V8 memory leak test

---
+++ scripts/developer/debugging/QObjectWrapperGCTest.js
@@ -0,0 +1,19 @@
+//
+//  scriptMemoryReport.js
+//  scripts/developer/debugging
+//
+//  Created by dr Karol Suprynowicz on 2023/04/23
+//  Copyright 2023 Overte e.V.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+// A simple script that prints memory usage statistics for a given script engine every 5 seconds
+// It can be included for example as a part of default scripts or controller scripts
+
+Script.setInterval(function () {
+    for (let i = 0; i < 50000; i++) {
+        let dbgobj = Script.createGarbageCollectorDebuggingObject();
+    }}, 10);
