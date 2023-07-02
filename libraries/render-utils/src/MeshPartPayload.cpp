vircadia/vircadia-native-core

wip finish transfering original PR

libraries/render-utils/src/MeshPartPayload.cpp
@@ -83,6 +83,7 @@ void MeshPartPayload::updateMeshPart(const std::shared_ptr<const graphics::Mesh>
 
 void MeshPartPayload::updateTransform(const Transform& transform, const Transform& offsetTransform) {
     _transform = transform;
+    _previousModelTransform = _drawTransform;
     Transform::mult(_drawTransform, _transform, offsetTransform);
     _worldBound = _localBound;
     _worldBound.transform(_drawTransform);
@@ -175,7 +176,7 @@ void MeshPartPayload::bindMesh(gpu::Batch& batch) {
 }
 
 void MeshPartPayload::bindTransform(gpu::Batch& batch, RenderArgs::RenderMode renderMode) const {
-    batch.setModelTransform(_drawTransform);
+    batch.setModelTransform(_drawTransform, _previousModelTransform);
 }
 
 bool MeshPartPayload::passesZoneOcclusionTest(const std::unordered_set<QUuid>& containingZones) const {
@@ -390,6 +391,7 @@ void ModelMeshPartPayload::updateClusterBuffer(const std::vector<Model::Transfor
 }
 
 void ModelMeshPartPayload::updateTransformForSkinnedMesh(const Transform& renderTransform, const Transform& boundTransform) {
+    _previousModelTransform = _transform;
     _transform = renderTransform;
     _worldBound = _adjustedLocalBound;
     _worldBound.transform(boundTransform);
@@ -497,7 +499,7 @@ void ModelMeshPartPayload::bindTransform(gpu::Batch& batch, RenderArgs::RenderMo
     if (_clusterBuffer) {
         batch.setUniformBuffer(graphics::slot::buffer::Skinning, _clusterBuffer);
     }
-    batch.setModelTransform(_transform);
+    batch.setModelTransform(_transform, _previousModelTransform);
 }
 
 void ModelMeshPartPayload::render(RenderArgs* args) {

overte-org/overte

libraries/render-utils/src/MeshPartPayload.cpp
@@ -162,6 +162,7 @@ void ModelMeshPartPayload::computeAdjustedLocalBound(const std::vector<Model::Tr
 }
 
 void ModelMeshPartPayload::updateTransformForSkinnedMesh(const Transform& modelTransform, const Model::MeshState& meshState, bool useDualQuaternionSkinning) {
+    _previousModelTransform = _transform;
     _localTransform = Transform();
     if (useDualQuaternionSkinning) {
         if (meshState.clusterDualQuaternions.size() == 1 || meshState.clusterDualQuaternions.size() == 2) {
@@ -192,7 +193,7 @@ void ModelMeshPartPayload::bindTransform(gpu::Batch& batch, const Transform& tra
     if (_clusterBuffer) {
         batch.setUniformBuffer(graphics::slot::buffer::Skinning, _clusterBuffer);
     }
-    batch.setModelTransform(transform);
+    batch.setModelTransform(transform, _previousModelTransform);
 }
 
 void ModelMeshPartPayload::drawCall(gpu::Batch& batch) const {