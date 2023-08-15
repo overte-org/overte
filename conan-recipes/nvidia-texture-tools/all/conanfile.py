14dad05190d0918f1dcfc7bcacd5bc1d30f5cad9
Edgar <Edgar@AnotherFoxGuy.com>
2023-08-15 12:32:53 +0200

    :package: Added nvidia-texture-tools pkg

---
+++ conan-recipes/nvidia-texture-tools/all/conanfile.py
@@ -0,0 +1,34 @@
+import os
+from conan import ConanFile
+from conan.tools.files import get, collect_libs
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+class NTTConan(ConanFile):
+    name = "nvidia-texture-tools"
+    license = "MIT"
+    url = "https://github.com/JulianGro/nvidia-texture-tools"
+    description = "Texture processing tools with support for Direct3D 10 and 11 formats."
+    settings = "os", "compiler", "build_type", "arch"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_TESTS"] = "OFF"
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = collect_libs(self)
