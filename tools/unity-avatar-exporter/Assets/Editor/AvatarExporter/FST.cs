1082e05d896bb9853e15d575d652a33f9de7a200
Edgar <Edgar@AnotherFoxGuy.com>
2023-08-24 13:50:53 +0200

    :page_facing_up: Added license to FST.cs

--- tools/unity-avatar-exporter/Assets/Editor/AvatarExporter/FST.cs
+++ tools/unity-avatar-exporter/Assets/Editor/AvatarExporter/FST.cs
@@ -1,5 +1,10 @@
-
-
+//  FST.cs
+//
+//  Created by Edgar on 24-8-2023
+//  Copyright 2023 Overte e.V.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
 using System;
 using System.Collections.Generic;
