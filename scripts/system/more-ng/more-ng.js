e6aa0c38fcd823148d143feac663ec0a6f15652e
ArmoredDragon <43324896+Armored-Dragon@users.noreply.github.com>
2023-08-26 02:59:19 -0500

    Refactor Increased reliability

--- scripts/system/more-ng/more-ng.js
+++ scripts/system/more-ng/more-ng.js
@@ -80,112 +80,153 @@ function onWebEventReceived(message) {
     }
 
     if (message.action === "install_script") {
-        logs(`Installing script ${message.data}`);
-        ScriptDiscoveryService.loadScript(message.data);
+        installScript(message.data);
     }
 
     if (message.action === "uninstall_script") {
-        logs(`Uninstalling script ${message.data}`);
-        ScriptDiscoveryService.stopScript(message.data);
+        uninstallScript(message.data);
     }
 }
 
-function getInstalledApps() {
-    const running_apps = ScriptDiscoveryService.getRunning();
-    const installed_apps = Settings.getValue("more_installed_list", []);
+function uninstallScript(url) {
+    logs(`Uninstalling script ${url}`);
+    ScriptDiscoveryService.stopScript(url);
+    let installed_apps = Settings.getValue("more_installed_list", []);
 
-    let return_array = [];
-    running_apps.forEach((app) => {
-        // REVIEW: Multiplatform support functional?
-        if (app.url.includes("file://")) return;
-        let found = false;
-
-        logs(`Processing ${app.name || app.title}`);
-        logs(app.url);
-
-        for (let i = 0; installed_apps.length > i; i++) {
-            if (installed_apps[i].url === app.url) {
-                return_array.push(installed_apps[i]);
-                found = true;
-                break;
-            }
-
-            // Our installed app is not saved in settings, add it
-            if (installed_apps.length === i) {
-                logs("Not in settings");
-                let template = {
-                    title: app.name,
-                    url: app.url,
-                    icon: app.icon || "",
-                    description: app.description,
-                    directory: app.directory,
-                    installed: true,
-                };
-                return_array.push(template);
-                found = true;
-                return;
-            }
+    for (let i = 0; installed_apps.length > i; i++) {
+        if (installed_apps[i].url === url) {
+            installed_apps.splice(i, 1);
+            Settings.setValue("more_installed_list", installed_apps);
+            break;
         }
-        if (!found) {
-            logs("Settings was blank; just pushing it in.");
-            let template = {
-                title: app.name,
-                url: app.url,
-                icon: app.icon || "",
-                description: app.description,
-                directory: app.directory,
-                installed: true,
-            };
-            return_array.push(template);
-            return;
-        }
-    });
-
-    Settings.setValue("more_installed_list", return_array);
-    _sendMessage({ action: "installed_apps", data: return_array });
+    }
 }
 
-function findThirdParty() {
+function installScript(url) {
+    logs(`Installing script ${url}`);
+    ScriptDiscoveryService.loadScript(url);
+    let installed_apps = Settings.getValue("more_installed_list", []);
+
+    app_info.thirdparty_apps.forEach((app) => {
+        if (app.url === url) {
+            // Update installed list to include newly installed app
+            logs("Updating installed apps list");
+            Settings.setValue("more_installed_list", [...installed_apps, app]);
+        }
+    });
+}
+
+function getInstalledApps() {
+    const installed_apps = Settings.getValue("more_installed_list", []);
+    const running_apps = ScriptDiscoveryService.getRunning();
+    let list = [];
+
+    list = installed_apps;
+
+    // logs(list);
+
+    running_apps.forEach((app) => {
+        let data_exists = false; // Do we have a formatted object saved in our settings?
+        //  Do not include local apps
+        if (app.url.includes("file://")) return;
+
+        // Check app in list against apps we have formatted entries for in Settings
+        list.forEach((existing_setting) => {
+            if (existing_setting.url === app.url) data_exists = true;
+        });
+
+        // We have saved data, no need to process it
+        if (data_exists) return;
+
+        let formatted_entry = {
+            title: app.name,
+            url: app.url,
+            icon: app.icon,
+            description: app.description,
+            directory: app.directory,
+            installed: true,
+        };
+
+        // Insert our entry for the running script
+        list.push(formatted_entry);
+    });
+
+    // Save our updated list
+    Settings.setValue("more_installed_list", list);
+    _sendMessage({ action: "installed_apps", data: list });
+}
+
+async function findThirdParty() {
+    // Clear third party app list
+    app_info.thirdparty_apps = [];
+
+    let requests = [];
+
     repo_list.forEach((url) => {
-        var req = new XMLHttpRequest();
+        requests.push(_getRequest(url));
+    });
 
-        req.requestComplete.connect(() => {
-            if (req.status !== 200) {
-                _sendMessage({
-                    action: "repo_loaded",
-                    data: { error: true, status: req.status },
-                });
-            }
+    const request_finished = await Promise.all(requests);
 
-            const response = JSON.parse(req.responseText);
-            let app_list_formatted = [];
+    request_finished.forEach((repo_data) => {
+        // REVIEW: Is this the best way to do it?
 
-            response.forEach((app) => {
-                const app_dir =
-                    url.split("/metadata.json")[0] + "/" + app.directory + "/";
+        repo_data.data.forEach((app) => {
+            let found = false;
+            let repo_valid_apps = [];
 
-                const icon_url = app_dir + app.icon;
-                const script_url = app_dir + app.script;
+            logs(app);
+            const app_dir =
+                repo_data.repo.split("/metadata.json")[0] +
+                "/" +
+                app.directory +
+                "/";
+            const icon_url = app_dir + app.icon;
+            const script_url = app_dir + app.script;
 
-                app_list_formatted.push({
-                    title: app.name,
-                    url: script_url,
-                    icon: icon_url,
-                    description: app.description,
-                    directory: app.directory,
-                    installed: false,
-                });
+            // Check if app is installed by URL
+            app_info.installed_apps.forEach((app) => {
+                if (app.url === script_url) found = true;
             });
+
+            // Already installed and in the list.
+            if (found) return;
+
+            let app_form = {
+                title: app.name,
+                url: script_url,
+                icon: icon_url,
+                description: app.description,
+                directory: app.directory,
+            };
+
+            repo_valid_apps.push(app_form);
+            // app_info.thirdparty_apps.push(app_form);
 
             return _sendMessage({
                 action: "repo_loaded",
-                data: app_list_formatted,
+                data: repo_valid_apps,
             });
-        });
 
-        req.open("GET", url);
-        req.send();
+            logs(icon_url);
+            logs(script_url);
+        });
     });
 }
+
+// TODO: Check for 404s or other errors
+async function _getRequest(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                resolve({ data: JSON.parse(xhr.response), repo: url });
+            }
+        };
+        xhr.send();
+    });
+}
+Settings.setValue("more_installed_list", []);
 
 repo_list = Settings.getValue("more_repo_list", repo_list);
