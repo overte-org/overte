e7277af238e8175ba72e93c8db33407fe33b7bfb
Armored-Dragon <forgejo3829105@armoreddragon.com>
2023-09-14 15:27:55 -0500

    inspect.js and mouselook.js compatibility

--- scripts/system/controllers/mouseLook.js
+++ scripts/system/controllers/mouseLook.js
@@ -18,6 +18,8 @@ by rampa3 (https://github.com/rampa3) and vegaslon (https://github.com/vegaslon)
 
     var tempOff = false;
 
+    var altMode = false;
+
     Camera.mouseLookChanged.connect(onMouseLookChanged);
 
     function onMouseLookChanged(newMouseLook) {
@@ -41,6 +43,21 @@ by rampa3 (https://github.com/rampa3) and vegaslon (https://github.com/vegaslon)
 
     function onKeyPressEvent(event) {
         if (!hmd){
+            if(event.isAlt){
+                if (keysOnOverlay) return;
+                if (!mouseLookEnabled) return;
+                mouseLookOff();
+                Window.displayAnnouncement("Mouse look: Temporarily OFF");
+                tempOff = true;
+                altMode = true;
+            }
+            if (tempOff && altMode && ['left', 'right', 'up', 'down', 'esc', 'w', 'a', 's', 'd'].includes(event.text.toLowerCase())){
+                if (keysOnOverlay) return;
+                if (!mouseLookEnabled) return;
+                mouseLookOn();
+                tempOff = false;
+                altMode = false
+            }
             if (event.text === 'm') {
                 if (!keysOnOverlay) {
                     if (mouseLookEnabled) {
