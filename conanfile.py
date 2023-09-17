06dff3ed3c6b0525584f10c5fbcc16b8a2b43624
Edgar <Edgar@AnotherFoxGuy.com>
2023-09-17 13:39:04 +0200

    :package: Added liblo pkg

--- conanfile.py
+++ conanfile.py
@@ -12,22 +12,29 @@ class Overte(ConanFile):
         self.requires("draco/1.5.6")
         self.requires("etc2comp/cci.20170424")
         self.requires("glad/0.1.36")
+        self.requires("gli/cci.20210515")
         self.requires("glm/cci.20230113")
-        self.requires("nodejs/18.15.0")
+        self.requires("glslang/11.7.0")
+        self.requires("liblo/0.30@overte/stable")
+        # libnode
+        self.requires("nlohmann_json/3.11.2")
+        self.requires("nvidia-texture-tools/2023.01@overte/stable")
         self.requires("openexr/3.1.9")
         self.requires("openssl/1.1.1t")
+        #self.requires("openvr/1.16.8") # Broken
         self.requires("opus/1.3.1")
-        # self.requires("tbb/2020.3")
-        self.requires("zlib/1.2.13")
-
-        self.requires("glslang/11.7.0")
-        self.requires("nlohmann_json/3.11.2")
-        # self.requires("openvr/1.16.8")
-        self.requires("quazip/1.4")
+        self.requires("polyvox/2016.11@overte/stable")
+        #self.requires("quazip/1.4")
         self.requires("sdl/2.26.5")
+        self.requires("scribe/2019.02@overte/stable")
+        #self.requires("shaderc/2021.1") # Broken
         self.requires("spirv-cross/cci.20211113")
         self.requires("spirv-tools/2021.4")
+        #self.requires("tbb/2020.3") # Broken
+        self.requires("v-hacd/4.1.0")
         self.requires("vulkan-memory-allocator/3.0.1")
+        # webrtc
+        self.requires("zlib/1.2.13")
 
     def generate(self):
         for dep in self.dependencies.values():
