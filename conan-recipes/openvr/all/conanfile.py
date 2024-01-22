5b2bc036e3a605488349eebbd7511464c5b3fc47
Edgar <Edgar@AnotherFoxGuy.com>
2024-01-22 11:23:28 +0100

    :bug: Fixed OpenVR failing to link on windows

--- conan-recipes/openvr/all/conanfile.py
+++ conan-recipes/openvr/all/conanfile.py
@@ -16,21 +16,40 @@ class OpenvrConan(ConanFile):
     homepage = "https://github.com/ValveSoftware/openvr"
     license = "BSD-3-Clause"
     settings = "os", "compiler", "build_type", "arch"
+    implements = ["auto_shared_fpic"]
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires("jsoncpp/1.9.4")
+        self.requires("jsoncpp/1.9.5")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
         # Unvendor jsoncpp (we rely on our CMake wrapper for jsoncpp injection)
-        replace_in_file(self, os.path.join(self.source_folder, "src", "CMakeLists.txt"), "jsoncpp.cpp", "")
+        rmdir(self, os.path.join(self.source_folder, "src", "json"))
+        replace_in_file(
+            self,
+            os.path.join(self.source_folder, "src", "CMakeLists.txt"),
+            "jsoncpp.cpp",
+            "",
+        )
+        replace_in_file(
+            self,
+            os.path.join(self.source_folder, "src", "CMakeLists.txt"),
+            "target_link_libraries(${LIBNAME} ${EXTRA_LIBS} ${CMAKE_DL_LIBS})",
+            """
+            find_package(jsoncpp)
+            target_link_libraries(${LIBNAME} JsonCpp::JsonCpp ${EXTRA_LIBS} ${CMAKE_DL_LIBS})
+            """,
+        )
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["BUILD_UNIVERSAL"] = "OFF"
+        tc.variables["BUILD_SHARED"] = self.options.shared
         tc.variables["USE_LIBCXX"] = "OFF"
         tc.generate()
         deps = CMakeDeps(self)
@@ -45,9 +64,17 @@ class OpenvrConan(ConanFile):
         copy(self, "LICENSE", src=self.source_folder, dst="licenses")
         cmake = CMake(self)
         cmake.install()
-        copy(self, pattern="openvr_api*.dll", dst="bin", src=os.path.join(self.source_folder, "bin"), keep_path=False)
+        copy(
+            self,
+            pattern="*.dll",
+            dst="bin",
+            src=os.path.join(self.build_folder, "bin", "win64"),
+            keep_path=False,
+        )
 
     def package_info(self):
+        if not self.options.shared:
+            self.cpp_info.defines = ["OPENVR_BUILD_STATIC"]
         self.cpp_info.names["pkg_config"] = "openvr"
         self.cpp_info.libs = collect_libs(self)
         self.cpp_info.includedirs.append(os.path.join("include", "openvr"))
