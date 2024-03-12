6bee693d0c4b431d5c1912102d7fbfecc6f11801
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2024-03-11 22:36:38 -0400

    Add "Copy ID"

--- scripts/system/create/entityList/entityList.js
+++ scripts/system/create/entityList/entityList.js
@@ -351,6 +351,8 @@ var EntityListTool = function(shouldUseEditTabletApp, selectionManager) {
             that.selectionManager.cutSelectedEntities();
         } else if (data.type === "copy") {
             that.selectionManager.copySelectedEntities();
+        } else if (data.type === "copyID") {
+            that.selectionManager.copyIdFromSelectedEntity();
         } else if (data.type === "paste") {
             that.selectionManager.pasteEntities();
         } else if (data.type === "duplicate") {
