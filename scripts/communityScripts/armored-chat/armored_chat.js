0a7b64d7be20bf547d2f74c73e339e8c01faf4c3
Armored Dragon <publicmail@armoreddragon.com>
2024-05-23 01:50:38 -0500

    Prevent VR quick_message bar.
    Signed-off-by: Armored Dragon <publicmail@armoreddragon.com>

--- scripts/communityScripts/armored-chat/armored_chat.js
+++ scripts/communityScripts/armored-chat/armored_chat.js
@@ -179,6 +179,8 @@
     function keyPressEvent(event) {
         switch (JSON.stringify(event.key)) {
             case "16777220": // Enter key
+                if (HMD.active) return; // Don't allow in VR
+
                 quick_message.sendToQml({
                     type: "change_visibility",
                     value: true,
