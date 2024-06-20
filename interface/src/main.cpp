ba0ab8dc5d83266684e8c5d92aa407e16cb34ab5
HifiExperiments <thingsandstuffblog@gmail.com>
2024-06-20 14:48:52 -0700

    fix audio devices + login popup

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -311,6 +311,7 @@ int main(int argc, const char* argv[]) {
     parser.addOption(noLauncherOption);
     parser.addOption(responseTokensOption);
     parser.addOption(displayNameOption);
+    parser.addOption(noLoginOption);
     parser.addOption(overrideScriptsPathOption);
     parser.addOption(defaultScriptsOverrideOption);
     parser.addOption(traceFileOption);
