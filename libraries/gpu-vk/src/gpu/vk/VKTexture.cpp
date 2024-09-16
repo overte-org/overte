b20346fd8594d292b5c626e67bd678a628112a12
Karol Suprynowicz <ksuprynowicz@post.pl>
2024-09-17 00:41:04 +0200

    Vulkan work on framebuffers and textures

--- libraries/gpu-vk/src/gpu/vk/VKTexture.cpp
+++ libraries/gpu-vk/src/gpu/vk/VKTexture.cpp
@@ -5,6 +5,369 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
+
+#include "VKTexture.h"
+
+#include <QtCore/QThread>
+#include <NumericalConstants.h>
+
+#include "VKBackend.h"
+
+using namespace gpu;
+using namespace gpu::vk;
+
+
+/*const uint32_t VKTexture::CUBE_FACE_LAYOUT[VKTexture::TEXTURE_CUBE_NUM_FACES] = {
+    GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+    GL_TEXTURE_CUBE_MAP_POSITIVE_Y, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+    GL_TEXTURE_CUBE_MAP_POSITIVE_Z, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+};*/
+
+const uint32_t VKTexture::WRAP_MODES[Sampler::NUM_WRAP_MODES] = {
+    VK_SAMPLER_ADDRESS_MODE_REPEAT,                         // WRAP_REPEAT,
+    VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT,                // WRAP_MIRROR,
+    VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE,                  // WRAP_CLAMP,
+    VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER,                // WRAP_BORDER,
+    VK_SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE        // WRAP_MIRROR_ONCE,
+};
+
+const VKFilterMode VKTexture::FILTER_MODES[Sampler::NUM_FILTERS] = {
+    { VK_FILTER_NEAREST, VK_FILTER_NEAREST },  //FILTER_MIN_MAG_POINT,
+    { VK_FILTER_NEAREST, VK_FILTER_LINEAR },  //FILTER_MIN_POINT_MAG_LINEAR,
+    { VK_FILTER_LINEAR, VK_FILTER_NEAREST },  //FILTER_MIN_LINEAR_MAG_POINT,
+    { VK_FILTER_LINEAR, VK_FILTER_LINEAR },  //FILTER_MIN_MAG_LINEAR,
+
+    // VKTODO: VkSamplerMipmapMode has some related setting too
+    // VKTODO: This is not really accurate, but hopefully good enough for now
+    { VK_FILTER_NEAREST, VK_FILTER_NEAREST },  //FILTER_MIN_MAG_MIP_POINT,
+    { VK_FILTER_LINEAR, VK_FILTER_NEAREST },  //FILTER_MIN_MAG_POINT_MIP_LINEAR,
+    { VK_FILTER_NEAREST, VK_FILTER_LINEAR },  //FILTER_MIN_POINT_MAG_LINEAR_MIP_POINT,
+    { VK_FILTER_LINEAR, VK_FILTER_LINEAR },  //FILTER_MIN_POINT_MAG_MIP_LINEAR,
+    { VK_FILTER_NEAREST, VK_FILTER_NEAREST },  //FILTER_MIN_LINEAR_MAG_MIP_POINT,
+    { VK_FILTER_LINEAR, VK_FILTER_NEAREST },  //FILTER_MIN_LINEAR_MAG_POINT_MIP_LINEAR,
+    { VK_FILTER_NEAREST, VK_FILTER_LINEAR },  //FILTER_MIN_MAG_LINEAR_MIP_POINT,
+    { VK_FILTER_LINEAR, VK_FILTER_LINEAR },  //FILTER_MIN_MAG_MIP_LINEAR,
+    { VK_FILTER_LINEAR, VK_FILTER_LINEAR }  //FILTER_ANISOTROPIC,
+};
+
+static constexpr size_t MAX_PIXEL_BYTE_SIZE{ 4 };
+static constexpr size_t MAX_TRANSFER_DIMENSION{ 1024 };
+
+/*const uvec3 GLVariableAllocationSupport::MAX_TRANSFER_DIMENSIONS{ MAX_TRANSFER_DIMENSION, MAX_TRANSFER_DIMENSION, 1 };
+const uvec3 GLVariableAllocationSupport::INITIAL_MIP_TRANSFER_DIMENSIONS{ 64, 64, 1 };
+const size_t GLVariableAllocationSupport::MAX_TRANSFER_SIZE = MAX_TRANSFER_DIMENSION * MAX_TRANSFER_DIMENSION * MAX_PIXEL_BYTE_SIZE;
+const size_t GLVariableAllocationSupport::MAX_BUFFER_SIZE = MAX_TRANSFER_SIZE;*/
+
+/*GLenum VKTexture::getVKTextureType(const Texture& texture) {
+    switch (texture.getType()) {
+        case Texture::TEX_2D:
+            if (!texture.isArray()) {
+                if (!texture.isMultisample()) {
+                    return GL_TEXTURE_2D;
+                } else {
+                    return GL_TEXTURE_2D_MULTISAMPLE;
+                }
+            } else {
+                if (!texture.isMultisample()) {
+                    return GL_TEXTURE_2D_ARRAY;
+                } else {
+                    return GL_TEXTURE_2D_MULTISAMPLE_ARRAY;
+                }
+            }
+            break;
+
+        case Texture::TEX_CUBE:
+            return GL_TEXTURE_CUBE_MAP;
+            break;
+
+        default:
+            qFatal("Unsupported texture type");
+    }
+    Q_UNREACHABLE();
+    return GL_TEXTURE_2D;
+}
+
+
+uint8_t VKTexture::getFaceCount(GLenum target) {
+    switch (target) {
+        case GL_TEXTURE_2D:
+        case GL_TEXTURE_2D_MULTISAMPLE:
+        case GL_TEXTURE_2D_ARRAY:
+        case GL_TEXTURE_2D_MULTISAMPLE_ARRAY:
+            return TEXTURE_2D_NUM_FACES;
+        case GL_TEXTURE_CUBE_MAP:
+            return TEXTURE_CUBE_NUM_FACES;
+        default:
+            Q_UNREACHABLE();
+            break;
+    }
+}
+const std::vector<GLenum>& VKTexture::getFaceTargets(GLenum target) {
+    static const std::vector<GLenum> cubeFaceTargets {
+        GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+        GL_TEXTURE_CUBE_MAP_POSITIVE_Y, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+        GL_TEXTURE_CUBE_MAP_POSITIVE_Z, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+    };
+    static const std::vector<GLenum> face2DTargets {
+        GL_TEXTURE_2D
+    };
+    static const std::vector<GLenum> face2DMSTargets{
+        GL_TEXTURE_2D_MULTISAMPLE
+    };
+    static const std::vector<GLenum> arrayFaceTargets{
+        GL_TEXTURE_2D_ARRAY
+    };
+    switch (target) {
+        case GL_TEXTURE_2D:
+            return face2DTargets;
+        case GL_TEXTURE_2D_MULTISAMPLE:
+            return face2DMSTargets;
+        case GL_TEXTURE_2D_ARRAY:
+            return arrayFaceTargets;
+        case GL_TEXTURE_CUBE_MAP:
+            return cubeFaceTargets;
+        default:
+            Q_UNREACHABLE();
+            break;
+    }
+    Q_UNREACHABLE();
+    return face2DTargets;
+}*/
+
+VKTexture::VKTexture(const std::weak_ptr<VKBackend>& backend, const Texture& texture, bool isTransferable) :
+    VKObject(*backend.lock(), texture),
+    _target(getVKTextureType(texture)),
+    _transferable(isTransferable),
+    _downsampleSource(backend),
+    _vkTexelFormat(evalTexelFormatInternal(texture.getTexelFormat()))
+{
+    Backend::setGPUObject(texture, this);
+}
+
+VKTexture::~VKTexture() {
+    auto backend = _backend.lock();
+    if (backend && _texture == VK_NULL_HANDLE) {
+        // VKTODO
+        // backend->releaseTexture(_id, 0);
+    }
+}
+
+VkImageViewType VKTexture::getVKTextureType(const Texture& texture) {
+    switch (texture.getType()) {
+        case Texture::TEX_2D:
+            if (!texture.isArray()) {
+                if (!texture.isMultisample()) {
+                    return VK_IMAGE_VIEW_TYPE_2D;
+                } else {
+                    // VKTODO: multisample?
+                    return VK_IMAGE_VIEW_TYPE_2D;
+                }
+            } else {
+                if (!texture.isMultisample()) {
+                    return VK_IMAGE_VIEW_TYPE_2D_ARRAY;
+                } else {
+                    // VKTODO multisample?
+                    return VK_IMAGE_VIEW_TYPE_2D_ARRAY;
+                }
+            }
+            break;
+
+        case Texture::TEX_CUBE:
+            return VK_IMAGE_VIEW_TYPE_CUBE;
+            break;
+
+        default:
+            qFatal("Unsupported texture type");
+    }
+    Q_UNREACHABLE();
+    return VK_IMAGE_VIEW_TYPE_2D;
+}
+
+// From VKS
+void VKAttachmentTexture::createTexture() {
+    VkImageCreateInfo imageCI = vks::initializers::imageCreateInfo();
+    imageCI.imageType = VK_IMAGE_TYPE_2D;
+    imageCI.format = evalTexelFormatInternal(_gpuObject.getTexelFormat());
+    imageCI.extent.width = _gpuObject.getWidth();
+    imageCI.extent.height = _gpuObject.getHeight();
+    imageCI.extent.depth = 1;
+    imageCI.mipLevels = 1;
+    imageCI.arrayLayers = _gpuObject.isArray() ? _gpuObject.getNumSlices() : 1;
+    imageCI.samples = VK_SAMPLE_COUNT_1_BIT;
+    imageCI.tiling = VK_IMAGE_TILING_OPTIMAL;
+    if (_gpuObject.isColorRenderTarget()) {
+        imageCI.usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    } else if (_gpuObject.isDepthStencilRenderTarget()) {
+        imageCI.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+    } else {
+        Q_ASSERT(false);
+    }
+
+    VkMemoryAllocateInfo memAlloc = vks::initializers::memoryAllocateInfo();
+    VkMemoryRequirements memReqs;
+
+    auto device = _backend.lock()->getContext().device->logicalDevice;
+
+    // Create image for this attachment
+    VK_CHECK_RESULT(vkCreateImage(device, &imageCI, nullptr, &vkImage));
+    vkGetImageMemoryRequirements(device, vkImage, &memReqs);
+    memAlloc.allocationSize = memReqs.size;
+    memAlloc.memoryTypeIndex = _backend.lock()->getContext().device->getMemoryType(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+    // VKTODO: this may need to be changed to VMA
+    VK_CHECK_RESULT(vkAllocateMemory(device, &memAlloc, nullptr, &vkDeviceMemory));
+    VK_CHECK_RESULT(vkBindImageMemory(device, vkImage, vkDeviceMemory, 0));
+
+    /*attachment.subresourceRange = {};
+    attachment.subresourceRange.aspectMask = aspectMask;
+    attachment.subresourceRange.levelCount = 1;
+    attachment.subresourceRange.layerCount = createinfo.layerCount;*/
+
+    /*VkImageViewCreateInfo imageView = vks::initializers::imageViewCreateInfo();
+    imageView.viewType = (_gpuObject.isArray()) ? VK_IMAGE_VIEW_TYPE_2D_ARRAY : VK_IMAGE_VIEW_TYPE_2D;
+    imageView.format = imageCI.format;
+    // VKTODO: what is subresource?
+    imageView.subresourceRange = attachment.subresourceRange;
+    imageView.subresourceRange.aspectMask = (attachment.hasDepth()) ? VK_IMAGE_ASPECT_DEPTH_BIT : aspectMask;
+    imageView.image = attachment.imageCI;
+    VK_CHECK_RESULT(vkCreateImageView(device, &imageView, nullptr, &attachment.view));*/
+
+}
+
+/*Size VKTexture::copyMipFaceFromTexture(uint16_t sourceMip, uint16_t targetMip, uint8_t face) const {
+    if (!_gpuObject.isStoredMipFaceAvailable(sourceMip)) {
+        return 0;
+    }
+    auto dim = _gpuObject.evalMipDimensions(sourceMip);
+    auto mipData = _gpuObject.accessStoredMipFace(sourceMip, face);
+    auto mipSize = _gpuObject.getStoredMipFaceSize(sourceMip, face);
+    if (mipData) {
+        GLTexelFormat texelFormat = GLTexelFormat::evalGLTexelFormat(_gpuObject.getTexelFormat(), _gpuObject.getStoredMipFormat());
+        return copyMipFaceLinesFromTexture(targetMip, face, dim, 0, texelFormat.internalFormat, texelFormat.format, texelFormat.type, mipSize, mipData->readData());
+    } else {
+        qCDebug(gpugllogging) << "Missing mipData level=" << sourceMip << " face=" << (int)face << " for texture " << _gpuObject.source().c_str();
+    }
+    return 0;
+}*/
+
+//VKTODO:
+/*GLExternalTexture::GLExternalTexture(const std::weak_ptr<GLBackend>& backend, const Texture& texture, GLuint id)
+    : Parent(backend, texture, id) {
+    Backend::textureExternalCount.increment();
+}
+
+GLExternalTexture::~GLExternalTexture() {
+    auto backend = _backend.lock();
+    if (backend) {
+        auto recycler = _gpuObject.getExternalRecycler();
+        if (recycler) {
+            backend->releaseExternalTexture(_id, recycler);
+        } else {
+            qCWarning(gpugllogging) << "No recycler available for texture " << _id << " possible leak";
+        }
+        const_cast<GLuint&>(_id) = 0;
+    }
+    Backend::textureExternalCount.decrement();
+}
+
+GLVariableAllocationSupport::GLVariableAllocationSupport() {
+    Backend::textureResourceCount.increment();
+}
+
+GLVariableAllocationSupport::~GLVariableAllocationSupport() {
+    Backend::textureResourceCount.decrement();
+    Backend::textureResourceGPUMemSize.update(_size, 0);
+    Backend::textureResourcePopulatedGPUMemSize.update(_populatedSize, 0);
+}
+
+void GLVariableAllocationSupport::incrementPopulatedSize(Size delta) const {
+    _populatedSize += delta;
+    // Keep the 2 code paths to be able to debug
+    if (_size < _populatedSize) {
+        Backend::textureResourcePopulatedGPUMemSize.update(0, delta);
+    } else  {
+        Backend::textureResourcePopulatedGPUMemSize.update(0, delta);
+    }
+}
+
+void GLVariableAllocationSupport::decrementPopulatedSize(Size delta) const {
+    _populatedSize -= delta;
+    // Keep the 2 code paths to be able to debug
+    if (_size < _populatedSize) {
+        Backend::textureResourcePopulatedGPUMemSize.update(delta, 0);
+    } else  {
+        Backend::textureResourcePopulatedGPUMemSize.update(delta, 0);
+    }
+}
+
+void GLVariableAllocationSupport::sanityCheck() const {
+    if (_populatedMip < _allocatedMip) {
+        qCWarning(gpugllogging) << "Invalid mip levels";
+    }
+}*/
+
+//VKTODO:
+/*TransferJob::TransferJob(const Texture& texture,
+                         uint16_t sourceMip,
+                         uint16_t targetMip,
+                         uint8_t face,
+                         uint32_t lines,
+                         uint32_t lineOffset) {
+    auto transferDimensions = texture.evalMipDimensions(sourceMip);
+    GLenum format;
+    GLenum internalFormat;
+    GLenum type;
+    GLTexelFormat texelFormat = GLTexelFormat::evalGLTexelFormat(texture.getTexelFormat(), texture.getStoredMipFormat());
+    format = texelFormat.format;
+    internalFormat = texelFormat.internalFormat;
+    type = texelFormat.type;
+    _transferSize = texture.getStoredMipFaceSize(sourceMip, face);
+
+    // If we're copying a subsection of the mip, do additional calculations to find the size and offset of the segment
+    if (0 != lines) {
+        transferDimensions.y = lines;
+        auto dimensions = texture.evalMipDimensions(sourceMip);
+        auto bytesPerLine = (uint32_t)_transferSize / dimensions.y;
+        _transferOffset = bytesPerLine * lineOffset;
+        _transferSize = bytesPerLine * lines;
+    }
+
+    Backend::texturePendingGPUTransferMemSize.update(0, _transferSize);
+
+    if (_transferSize > GLVariableAllocationSupport::MAX_TRANSFER_SIZE) {
+        qCWarning(gpugllogging) << "Transfer size of " << _transferSize << " exceeds theoretical maximum transfer size";
+    }
+
+    // Buffering can invoke disk IO, so it should be off of the main and render threads
+    _bufferingLambda = [=](const TexturePointer& texture) {
+        auto mipStorage = texture->accessStoredMipFace(sourceMip, face);
+        if (mipStorage) {
+            _mipData = mipStorage->createView(_transferSize, _transferOffset);
+        } else {
+            qCWarning(gpugllogging) << "Buffering failed because mip could not be retrieved from texture "
+                                    << texture->source().c_str();
+        }
+    };
+
+    _transferLambda = [=](const TexturePointer& texture) {
+        if (_mipData) {
+            auto gltexture = Backend::getGPUObject<VKTexture>(*texture);
+            gltexture->copyMipFaceLinesFromTexture(targetMip, face, transferDimensions, lineOffset, internalFormat, format,
+                                                   type, _mipData->size(), _mipData->readData());
+            _mipData.reset();
+        } else {
+            qCWarning(gpugllogging) << "Transfer failed because mip could not be retrieved from texture "
+                                    << texture->source().c_str();
+        }
+    };
+}
+
+TransferJob::TransferJob(uint16_t sourceMip, const std::function<void()>& transferLambda) :
+    _sourceMip(sourceMip), _bufferingRequired(false), _transferLambda([=](const TexturePointer&) { transferLambda(); }) {}
+
+TransferJob::~TransferJob() {
+    Backend::texturePendingGPUTransferMemSize.update(_transferSize, 0);
+}*/
+
 #if 0
 #include "VKTexture.h"
 
--- libraries/gpu-vk/src/gpu/vk/VKTexture.h
+++ libraries/gpu-vk/src/gpu/vk/VKTexture.h
@@ -9,7 +9,7 @@
 #define hifi_gpu_vk_VKTexture_h
 
 #include "VKShared.h"
-#include "VKTextureTransfer.h"
+//#include "VKTextureTransfer.h"
 #include "VKBackend.h"
 
 namespace gpu { namespace vk {
@@ -19,10 +19,11 @@ struct VKFilterMode {
     VkFilter magFilter;
 };
 
-class VKTexture : public VKObject<Texture, VkImage> {
+class VKTexture : public VKObject<Texture> {
 public:
-    static void initTextureTransferHelper();
-    static std::shared_ptr<VKTextureTransferHelper> _textureTransferHelper;
+    // VKTODO
+    //static void initTextureTransferHelper();
+    //static std::shared_ptr<VKTextureTransferHelper> _textureTransferHelper;
 
     template <typename VKTextureType>
     static VKTextureType* sync(VKBackend& backend, const TexturePointer& texturePointer, bool needTransfer) {
@@ -40,15 +41,15 @@ public:
         if (!object || object->isInvalid()) {
             // This automatically any previous texture
             object = new VKTextureType(backend.shared_from_this(), texture, needTransfer);
-            if (!object->_transferrable) {
+            if (!object->_transferable) {
                 object->createTexture();
                 object->_contentStamp = texture.getDataStamp();
                 object->postTransfer();
             }
         }
 
-        // Object maybe doens't neet to be tranasferred after creation
-        if (!object->_transferrable) {
+        // Object maybe doesn't need to be transferred after creation
+        if (!object->_transferable) {
             return object;
         }
 
@@ -64,21 +65,23 @@ public:
                 // WARNING, this code path will essentially `delete this`, 
                 // so no dereferencing of this instance should be done past this point
                 object = new VKTextureType(backend.shared_from_this(), texture, object);
-                _textureTransferHelper->transferTexture(texturePointer);
+                // VKTODO
+                // _textureTransferHelper->transferTexture(texturePointer);
             }
         } else if (object->isOutdated()) {
             // Object might be outdated, if so, start the transfer
             // (outdated objects that are already in transfer will have reported 'true' for ready()
-            _textureTransferHelper->transferTexture(texturePointer);
+            // VKTODO
+            // _textureTransferHelper->transferTexture(texturePointer);
         }
 
         return object;
     }
 
     template <typename VKTextureType> 
-    static VkImage getId(VKBackend& backend, const TexturePointer& texture, bool shouldSync) {
+    static VkImage getHandle(VKBackend& backend, const TexturePointer& texture, bool shouldSync) {
         if (!texture) {
-            return 0;
+            return VK_NULL_HANDLE;
         }
         VKTextureType* object { nullptr };
         if (shouldSync) {
@@ -87,16 +90,16 @@ public:
             object = Backend::getGPUObject<VKTextureType>(*texture);
         }
         if (!object) {
-            return 0;
+            return VK_NULL_HANDLE;
         }
 
-        VKuint result = object->_id;
+        VkImage result = object->_id;
 
         // Don't return textures that are in transfer state
         if (shouldSync) {
             if ((object->getSyncState() != VKSyncState::Idle) ||
-                // Don't return transferrable textures that have never completed transfer
-                (!object->_transferrable || 0 != object->_transferCount)) {
+                // Don't return transferable textures that have never completed transfer
+                (!object->_transferable || 0 != object->_transferCount)) {
                 // Will be either 0 or the original texture being downsampled.
                 result = object->_downsampleSource._texture;
             }
@@ -106,97 +109,144 @@ public:
     }
 
     // Used by derived classes and helpers to ensure the actual VK object exceeds the lifetime of `this`
-    VkImage takeOwnership() {
+    /*VkImage takeOwnership() {
         VkImage result = _id;
         const_cast<VkImage&>(_id) = 0;
         return result;
-    }
+    }*/
 
-    ~VKTexture();
+    virtual ~VKTexture();
 
-    const Stamp _storageStamp;
-    const VkImageType _target;
-    const uint16 _maxMip;
-    const uint16 _minMip;
-    const Size _virtualSize; // theoretical size as expected
+    VkImage _texture { VK_NULL_HANDLE };
+    //const Stamp _storageStamp;
+    const VkImageViewType _target;
+    //const uint16 _maxMip;
+    //const uint16 _minMip;
+    //const Size _virtualSize; // theoretical size as expected
     Stamp _contentStamp { 0 };
-    const bool _transferrable;
+    const bool _transferable;
     Size _transferCount { 0 };
 
     struct DownsampleSource {
         using Pointer = std::shared_ptr<DownsampleSource>;
-        DownsampleSource(const std::weak_ptr<vk::VKBackend>& backend) : _backend(backend), _size(0), _texture(0), _minMip(0), _maxMip(0) {}
-        DownsampleSource(const std::weak_ptr<vk::VKBackend>& backend, VKTexture* originalTexture);
-        ~DownsampleSource();
-        void reset() const { const_cast<VKuint&>(_texture) = 0; }
+        DownsampleSource(const std::weak_ptr<VKBackend>& backend) : _backend(backend), _size(0), _texture(0), _minMip(0), _maxMip(0) {}
+        DownsampleSource(const std::weak_ptr<VKBackend>& backend, VKTexture* originalTexture)  : _backend(backend), _size(0), _texture(0), _minMip(0), _maxMip(0) { Q_ASSERT(false); }; // VKTODO
+        ~DownsampleSource() {}; // VKTODO
+        void reset() { _texture = VK_NULL_HANDLE; }
         const std::weak_ptr<vk::VKBackend>& _backend;
         const Size _size { 0 };
         // TODO: Shouldn't that be texture instead?
-        const VkImage _texture { 0 };
+        VkImage _texture { VK_NULL_HANDLE };
         const uint16 _minMip { 0 };
         const uint16 _maxMip { 0 };
     } _downsampleSource;
 
-    VKuint size() const { return _size; }
+    virtual size_t size() const { return _size; }
     VKSyncState getSyncState() const { return _syncState; }
 
     // Is the storage out of date relative to the gpu texture?
-    bool isInvalid() const;
+    // VKTODO
+    bool isInvalid() const { return false; };
 
     // Is the content out of date relative to the gpu texture?
-    bool isOutdated() const;
+    // VKTODO
+    bool isOutdated() const { return false; };
 
     // Is the texture in a state where it can be rendered with no work?
-    bool isReady() const;
+    bool isReady() const {
+        // VKTODO: Is this right?
+        return _syncState == VKSyncState::Transferred;
+    };
 
     // Execute any post-move operations that must occur only on the main thread
-    void postTransfer();
+    virtual void postTransfer() = 0;
 
-    bool isOverMaxMemory() const;
+    // VKTODO: can be done later
+    bool isOverMaxMemory() const { return false; };
 
 protected:
     static const size_t CUBE_NUM_FACES = 6;
-    static const VKenum CUBE_FACE_LAYOUT[6];
+    // VKTODO
+    //static const uint8_t TEXTURE_CUBE_NUM_FACES = 6;
+    //static const uint32_t CUBE_FACE_LAYOUT[TEXTURE_CUBE_NUM_FACES];
     static const VKFilterMode FILTER_MODES[Sampler::NUM_FILTERS];
-    static const VKenum WRAP_MODES[Sampler::NUM_WRAP_MODES];
+    static const uint32_t WRAP_MODES[Sampler::NUM_WRAP_MODES];
 
-    static const std::vector<VKenum>& getFaceTargets(VKenum textureType);
+    //static const std::vector<VKenum>& getFaceTargets(VKenum textureType);
 
-    static vk::ImageType getGLTextureType(const Texture& texture);
+    static VkImageViewType getVKTextureType(const Texture& texture);
     // Return a floating point value indicating how much of the allowed 
     // texture memory we are currently consuming.  A value of 0 indicates 
     // no texture memory usage, while a value of 1 indicates all available / allowed memory
     // is consumed.  A value above 1 indicates that there is a problem.
-    static float getMemoryPressure();
+    // VKTODO: this can wait for now
+    static float getMemoryPressure() { return 0; };
 
 
-    const VKuint _size { 0 }; // true size as reported by the gl api
+    const size_t _size { 0 }; // true size as reported by the Vulkan api
     std::atomic<VKSyncState> _syncState { VKSyncState::Idle };
+    VkFormat _vkTexelFormat { VK_FORMAT_UNDEFINED };
 
-    VKTexture(const std::weak_ptr<vk::VKBackend>& backend, const Texture& texture, VKuint id, bool transferrable);
-    VKTexture(const std::weak_ptr<vk::VKBackend>& backend, const Texture& texture, VKuint id, VKTexture* originalTexture);
+    VKTexture(const std::weak_ptr<VKBackend>& backend, const Texture& texture, bool isTransferable);
+    //VKTexture(const std::weak_ptr<VKBackend>& backend, const Texture& texture, VkImage image, bool transferrable);
+    //VKTexture(const std::weak_ptr<VKBackend>& backend, const Texture& texture, VkImage image, VKTexture* originalTexture);
 
     void setSyncState(VKSyncState syncState) { _syncState = syncState; }
-    uint16 usedMipLevels() const { return (_maxMip - _minMip) + 1; }
+    //uint16 usedMipLevels() const { return (_maxMip - _minMip) + 1; }
 
-    void createTexture();
-    
+    virtual void createTexture() = 0;
+
     virtual void allocateStorage() const = 0;
     virtual void updateSize() const = 0;
     virtual void transfer() const = 0;
     virtual void syncSampler() const = 0;
-    virtual void generateMips() const = 0;
-    virtual void withPreservedTexture(std::function<void()> f) const = 0;
+    // VKTODO
+    //virtual void generateMips() const = 0;
+    // VKTODO what does this mean?
+    //virtual void withPreservedTexture(std::function<void()> f) const = 0;
 
 protected:
-    void setSize(VKuint size) const;
+    void setSize(size_t size) const;
 
 private:
 
-    VKTexture(const std::weak_ptr<VKBackend>& backend, const gpu::Texture& gpuTexture, VKuint id, VKTexture* originalTexture, bool transferrable);
+    VKTexture(const std::weak_ptr<VKBackend>& backend, const gpu::Texture& gpuTexture, VkImage image, VKTexture* originalTexture, bool transferrable);
 
     friend class VKTextureTransferHelper;
     friend class VKBackend;
+};
+
+class VKFixedAllocationTexture : public VKTexture {
+    using Parent = VKTexture;
+    friend class GL45Backend;
+
+public:
+    VKFixedAllocationTexture(const std::weak_ptr<VKBackend>& backend, const Texture& texture, bool isTransferable) :
+        VKTexture(backend, texture, isTransferable) {}; // VKTODO
+    virtual ~VKFixedAllocationTexture() {};
+
+protected:
+    Size size() const override { return _size; }
+
+    void allocateStorage() const;
+    void syncSampler() const override;
+    void updateSize() const override;
+    const Size _size{ 0 };
+};
+
+class VKAttachmentTexture : public VKFixedAllocationTexture {
+    using Parent = VKFixedAllocationTexture;
+    friend class VKBackend;
+
+protected:
+    VKAttachmentTexture(const std::weak_ptr<VKBackend>& backend, const Texture& texture) :
+        VKFixedAllocationTexture(backend, texture, false) {};
+    virtual ~VKAttachmentTexture() {}; // VKTODO: delete image and image view, release memory
+    void createTexture() override;
+    void transfer() const override {}; // VKTODO
+    void postTransfer() override {}; // VKTODO
+    VkImage vkImage { VK_NULL_HANDLE };
+    VkDeviceMemory vkDeviceMemory { VK_NULL_HANDLE };
 };
 
 } }
