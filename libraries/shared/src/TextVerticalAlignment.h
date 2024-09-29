470cd776cc47fafe723fd0c9ad10ce069980a843
HifiExperiments <thingsandstuffblog@gmail.com>
2024-09-29 16:10:44 -0700

    fix copyright

--- libraries/shared/src/TextVerticalAlignment.h
+++ libraries/shared/src/TextVerticalAlignment.h
@@ -1,6 +1,6 @@
 //
 //  Created by HifiExperiments on 8/17/24
-//  Copyright 2021 Vircadia contributors.
+//  Copyright 2024 Overte e.V contributors.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
