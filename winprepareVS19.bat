b32372ea6b92eebf8ace116f1cbd4f611102f67b
Edgar <Edgar@AnotherFoxGuy.com>
2024-11-01 12:48:04 +0100

    :memo: Updated documentation

--- winprepareVS19.bat
+++ winprepareVS19.bat
@@ -1,6 +1,7 @@
 ECHO Running conan
 conan install . -b missing -pr=tools/conan-profiles/vs-19-release -of build
 conan install . -b missing -pr=tools/conan-profiles/vs-19-debug -of build
+conan cache clean "*" -sbd
 ECHO Running cmake
 cmake --preset conan-default
 ECHO CMake has finished
