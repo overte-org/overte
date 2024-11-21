3485ab18d124f059ca7bcf7f0bf67623484263f0
Karol Suprynowicz <ksuprynowicz@post.pl>
2024-11-21 20:08:25 +0100

    Added copyrigtht information to ScriptEngineDebugFlags.h

--- libraries/script-engine/src/v8/ScriptEngineDebugFlags.h
+++ libraries/script-engine/src/v8/ScriptEngineDebugFlags.h
@@ -1,10 +1,18 @@
 //
-// Created by ksuprynowicz on 14.11.24.
+//  ScriptEngineDebugFlags.h
+//  libraries/script-engine/src/v8/ScriptEngineDebugFlags.h
+//
+//  Created by dr Karol Suprynowicz on 2024/11/14.
+//  Copyright 2024 Overte e.V.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//  SPDX-License-Identifier: Apache-2.0
 //
 
-#ifndef OVERTE_SCRIPTENGINEDEBUGFLAGS_H
-#define OVERTE_SCRIPTENGINEDEBUGFLAGS_H
+#ifndef overte_ScriptEngineDebugFlags_h
+#define overte_ScriptEngineDebugFlags_h
 
 #define OVERTE_SCRIPT_USE_AFTER_DELETE_GUARD
 
-#endif  //OVERTE_SCRIPTENGINEDEBUGFLAGS_H
+#endif
