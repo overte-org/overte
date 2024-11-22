3c92c881569aba2316b7d41ee1e9fa1545c93d35
Armored-Dragon <github56254@armoreddragon.com>
2024-11-21 19:04:01 -0600

    Adjustments
    * Adjustments
    * Don't show local stats.

--- libraries/avatars-renderer/src/avatars-renderer/Avatar.cpp
+++ libraries/avatars-renderer/src/avatars-renderer/Avatar.cpp
@@ -994,9 +994,9 @@ void Avatar::renderDisplayName(gpu::Batch& batch, const ViewFrustum& view, const
 
         QString statsFormat = QString("(%1 Kbps, %2 Hz)");
         if (!renderedDisplayName.isEmpty()) {
-            statsFormat.prepend(" - ");
+            statsFormat.append("\n");
         }
-        renderedDisplayName += statsFormat.arg(QString::number(kilobitsPerSecond, 'f', 2)).arg(getReceiveRate());
+        renderedDisplayName = statsFormat.arg(QString::number(kilobitsPerSecond, 'f', 2)).arg(getReceiveRate())  + renderedDisplayName;
     }
 
     // Compute display name extent/position offset
@@ -1020,8 +1020,7 @@ void Avatar::renderDisplayName(gpu::Batch& batch, const ViewFrustum& view, const
 
         // Display name and background colors
         glm::vec4 textColor(0.93f, 0.93f, 0.93f, _displayNameAlpha);
-        glm::vec4 backgroundColor(0.2f, 0.2f, 0.2f,
-                                  (_displayNameAlpha / DISPLAYNAME_ALPHA) * DISPLAYNAME_BACKGROUND_ALPHA);
+        glm::vec4 backgroundColor(0.2f, 0.2f, 0.2f,(_displayNameAlpha / DISPLAYNAME_ALPHA) * DISPLAYNAME_BACKGROUND_ALPHA);
 
         // Compute display name transform
         auto textTransform = calculateDisplayNameTransform(view, textPosition);
@@ -1029,12 +1028,11 @@ void Avatar::renderDisplayName(gpu::Batch& batch, const ViewFrustum& view, const
         textTransform.postScale(1.0f / height);
         batch.setModelTransform(textTransform);
 
-        {
-            PROFILE_RANGE_BATCH(batch, __FUNCTION__":renderBevelCornersRect");
-            DependencyManager::get<GeometryCache>()->bindSimpleProgram(batch, false, false, true, true, true, forward);
-            DependencyManager::get<GeometryCache>()->renderBevelCornersRect(batch, left, bottom, width, height,
-                bevelDistance, backgroundColor, _nameRectGeometryID);
-        }
+        // {
+        //     PROFILE_RANGE_BATCH(batch, __FUNCTION__":renderBevelCornersRect");
+        //     DependencyManager::get<GeometryCache>()->bindSimpleProgram(batch, false, false, true, true, true, forward);
+        //     DependencyManager::get<GeometryCache>()->renderBevelCornersRect(batch, left, bottom, width, height, bevelDistance, backgroundColor, _nameRectGeometryID);
+        // }
 
         // Render actual name
         QByteArray nameUTF8 = renderedDisplayName.toLocal8Bit();
@@ -1044,7 +1042,7 @@ void Avatar::renderDisplayName(gpu::Batch& batch, const ViewFrustum& view, const
         batch.setModelTransform(textTransform);
         {
             PROFILE_RANGE_BATCH(batch, __FUNCTION__":renderText");
-            displayNameRenderer->draw(batch, { nameUTF8.data(), textColor, { text_x, -text_y }, glm::vec2(-1.0f), forward });
+            displayNameRenderer->draw(batch, { nameUTF8.data(), textColor, { text_x, -text_y }, glm::vec2(10.0f), forward, TextAlignment::CENTER });
         }
     }
 }
