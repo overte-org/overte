2e37ac11b6a403b4d66d901ca637d620f3be4af7
ksuprynowicz <ksuprynowicz@post.pl>
2024-12-15 10:39:21 +0100

    Vulkan backend cleanup

--- libraries/vk/src/vk/Forward.h
+++ libraries/vk/src/vk/Forward.h
@@ -1,7 +1,4 @@
 namespace vks {
-    struct Allocated;
     struct Buffer;
-    struct Image;
     struct Context;
-    struct Swapchain;
 }
--- libraries/vk/src/vk/VulkanDevice.h
+++ libraries/vk/src/vk/VulkanDevice.h
@@ -58,13 +58,9 @@ struct VulkanDevice
     uint32_t        getMemoryType(uint32_t typeBits, VkMemoryPropertyFlags properties, VkBool32 *memTypeFound = nullptr) const;
     uint32_t        getQueueFamilyIndex(VkQueueFlags queueFlags) const;
     VkResult        createLogicalDevice(VkPhysicalDeviceFeatures enabledFeatures, std::vector<const char *> enabledExtensions, void *pNextChain, bool useSwapChain = true, VkQueueFlags requestedQueueTypes = VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT);
-    VkResult        createBuffer(VkBufferUsageFlags usageFlags, VkMemoryPropertyFlags memoryPropertyFlags, VkDeviceSize size, VkBuffer *buffer, VkDeviceMemory *memory, void *data = nullptr);
-    VkResult        createBuffer(VkBufferUsageFlags usageFlags, VkMemoryPropertyFlags memoryPropertyFlags, vks::Buffer *buffer, VkDeviceSize size, void *data = nullptr);
-    void            copyBuffer(vks::Buffer *src, vks::Buffer *dst, VkQueue queue, VkBufferCopy *copyRegion = nullptr);
     VkCommandPool   createCommandPool(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags createFlags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
     VkCommandBuffer createCommandBuffer(VkCommandPool pool, VkCommandBufferLevel level, bool begin = false);
     void            flushCommandBuffer(VkCommandBuffer commandBuffer, VkQueue queue, VkCommandPool pool, bool free = true);
     bool            extensionSupported(std::string extension);
-    VkFormat        getSupportedDepthFormat(bool checkSamplingSupport);
 };
 }        // namespace vks
