82541bda499919a0633be3172928c5444723e54e
Julian Groß <julian.g@posteo.de>
2025-02-19 18:23:26 +0100

    Build libnode with llhttp provided by conan.

--- conanfile.py
+++ conanfile.py
@@ -56,7 +56,7 @@ class Overte(ConanFile):
         self.requires("gli/cci.20210515")
         self.requires("glslang/11.7.0")
         self.requires("liblo/0.30@overte/stable")
-        self.requires("libnode/18.20.6@overte/stable")
+        self.requires("libnode/18.20.6@overte/stable#85a7e34916bfaa1b699f59f2e8721e9e")
         self.requires("nlohmann_json/3.11.2")
         self.requires("nvidia-texture-tools/2023.01@overte/stable")
         self.requires("onetbb/2021.10.0")
