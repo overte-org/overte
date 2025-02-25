b43ed66bf395549f9543d58f4f70d5eea1b1d748
Ada <ada+github@thingvellir.net>
2025-02-26 04:55:14 +1000

    Canvas frame update experiment

--- libraries/entities-renderer/src/RenderableCanvasEntityItem.cpp
+++ libraries/entities-renderer/src/RenderableCanvasEntityItem.cpp
@@ -14,7 +14,7 @@ using namespace render::entities;
 CanvasEntityRenderer::CanvasEntityRenderer(const EntityItemPointer& entity) : Parent(entity) {
     gpu::Byte pixels[256 * 256 * 4];
 
-    // grayscale XOR placeholder texture
+    // dummy placeholder texture to make sure the per-frame thing works
     for (int x = 0; x < 256; x++) {
         for (int y = 0; y < 256; y++) {
             pixels[(y * 256 * 4) + (x * 4) + 0] = 255;
@@ -28,6 +28,29 @@ CanvasEntityRenderer::CanvasEntityRenderer(const EntityItemPointer& entity) : Pa
     _texture->setStoredMipFormat(gpu::Element::COLOR_SRGBA_32);
     _texture->assignStoredMip(0, 256 * 256 * 4, pixels);
     _texture->setSource(__FUNCTION__);
+
+    _testTimer.setInterval(33);
+    connect(&_testTimer, &QTimer::timeout, this, &CanvasEntityRenderer::onTimeout);
+    _testTimer.start();
 }
 
 CanvasEntityRenderer::~CanvasEntityRenderer() { }
+
+void CanvasEntityRenderer::onTimeout() {
+    gpu::Byte pixels[256 * 256 * 4];
+
+    // XOR placeholder texture
+    for (int x = 0; x < 256; x++) {
+        for (int y = 0; y < 256; y++) {
+            pixels[(y * 256 * 4) + (x * 4) + 0] = ((x + _ticks) ^ (y + _ticks));
+            pixels[(y * 256 * 4) + (x * 4) + 1] = ((x + _ticks) ^ (y + _ticks));
+            pixels[(y * 256 * 4) + (x * 4) + 2] = ((x + _ticks) ^ (y + _ticks));
+            pixels[(y * 256 * 4) + (x * 4) + 3] = 255;
+        }
+    }
+
+    _texture->assignStoredMip(0, 256 * 256 * 4, pixels);
+
+    _ticks += 1;
+    qDebug("onTimeout: _ticks = %d", _ticks);
+}
