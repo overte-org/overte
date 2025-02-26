ea7cc22e3c6b26330be6b823e139fe972c8d3df2
Julian Groß <julian.g@posteo.de>
2025-02-26 15:20:58 +0100

    Move Conan recipes to https://github.com/overte-org/overte-conan-recipes

--- conan-recipes/nss/all/test_package/build/gcc-14-x86_64-gnu17-release/generators/deactivate_conanrunenv-release-x86_64.sh
+++
@@ -1,4 +0,0 @@
-echo Restoring environment
-export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/snap/bin'
-export LD_LIBRARY_PATH=''
-unset DYLD_LIBRARY_PATH
--- conan-recipes/qt/5.x.x/test_package/build/gcc-14-x86_64-gnu17-release/generators/deactivate_conanbuildenv-release-x86_64.sh
+++
@@ -1,8 +0,0 @@
-echo Restoring environment
-unset ACLOCAL_PATH
-unset AUTOMAKE_CONAN_INCLUDES
-unset AUTOCONF
-unset AUTORECONF
-unset AUTOHEADER
-unset AUTOM4TE
-unset M4
