4ff906a9bd1effbb11abd247b7f95721bf6fc897
Ada <ada+github@thingvellir.net>
2025-02-27 07:50:49 +1000

    Canvas basic image submission fully working

--- libraries/entities/src/EntityScriptingInterface.cpp
+++ libraries/entities/src/EntityScriptingInterface.cpp
@@ -2709,7 +2709,3 @@ void EntityScriptingInterface::canvasSubmitImage(const QUuid& entityID, const QB
         qCWarning(entities) << "canvasSubmitImage called on a non-canvas entity " << entityID;
     }
 }
-
-void EntityScriptingInterface::canvasSubmitSubImage(const QUuid& entityID, const QByteArray& imageData, const QVector<uint32_t>& destRect, const QVector<uint32_t>& srcRect) {
-    qCWarning(entities) << "canvasSubmitSubImage unimplemented! called on " << entityID;
-}
