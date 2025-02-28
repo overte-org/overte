74ca7328913da5807ef63550d67bba63c5b268a1
Ada <ada+github@thingvellir.net>
2025-02-28 23:47:25 +1000

    CanvasCommand better type safety and accessible from script, segfault on usage

--- libraries/script-engine/src/ScriptManager.cpp
+++ libraries/script-engine/src/ScriptManager.cpp
@@ -847,6 +847,10 @@ void ScriptManager::init() {
     scriptEngine->registerGlobalObject("StackTest", new StackTestScriptingInterface(this));
 #endif
 
+    if (_context == CLIENT_SCRIPT) {
+        scriptEngine->registerGlobalObject("CanvasCommand", &_canvasCommandLibrary);
+    }
+
     qCDebug(scriptengine) << "Engine initialized";
 }
 
