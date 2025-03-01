785418ec20eeaffa2c6a57e97ec056d371e218cd
Julian Groß <julian.g@posteo.de>
2025-03-01 18:22:14 +0100

    Patch glslang for GCC 15 compatibility.

--- cmake/ports/glslang/portfile.cmake
+++ cmake/ports/glslang/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
   REF 11.13.0
   SHA512 20c2a6543b002648f459f26bd36b5c445afd6d8eae175e400dbe45632f11ca8de1f9e6f6e98fd6f910aa75d90063e174c095e7df26d9d4982192b84d08b0dc8b
   HEAD_REF master
-  PATCHES 0001-Include-cstdint-header-in-Common.h.patch
+  PATCHES 0001-Include-cstdint-header-in-Common.h.patch 0001-SPIRV-SpvBuilder.h-add-missing-cstdint-include.patch
 )
 
 vcpkg_configure_cmake(
