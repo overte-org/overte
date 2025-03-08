98b65f4af25948a869246e927a11373b2dac5a4e
Julian Groß <julian.g@posteo.de>
2025-03-08 22:47:40 +0100

    Remove VCPKG references.

--- cmake/macros/TargetJson.cmake
+++ cmake/macros/TargetJson.cmake
@@ -1,15 +1,12 @@
 # 
 #  Created by Bradley Austin Davis on 2018/07/22
 #  Copyright 2013-2018 High Fidelity, Inc.
+#  Copyright 2025 Overte e.V.
 #
 #  Distributed under the Apache License, Version 2.0.
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 macro(TARGET_JSON)
-	# We use vcpkg for both json and glm, so we just re-use the target_glm macro here
-    # target_glm()
-
-
     find_package(nlohmann_json REQUIRED)
     target_link_libraries(${TARGET_NAME} nlohmann_json::nlohmann_json)
 endmacro()
