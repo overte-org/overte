b95fb3013c770c81b620fdba5d25311154b1a577
HifiExperiments <thingsandstuffblog@gmail.com>
2025-03-16 14:32:01 -0700

    why

--- interface/src/ui/Stats.cpp
+++ interface/src/ui/Stats.cpp
@@ -425,7 +425,7 @@ void Stats::updateStats(bool force) {
     gpu::ContextStats gpuFrameStats;
     gpuContext->getFrameStats(gpuFrameStats);
 
-    STAT_UPDATE(drawcalls, gpuFrameStats._DSNumDrawcalls);
+    STAT_UPDATE(drawcalls, (quint32)gpuFrameStats._DSNumDrawcalls);
     STAT_UPDATE(lodTargetFramerate, DependencyManager::get<LODManager>()->getLODTargetFPS());
     STAT_UPDATE(lodAngle, DependencyManager::get<LODManager>()->getLODAngleDeg());
 
