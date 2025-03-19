b233d2b03e83f846547ddf67292af08b409bf8df
Julian Groß <julian.g@posteo.de>
2025-03-20 00:59:49 +0100

    Remove VCPKG specific code.

--- cmake/macros/TargetGlm.cmake
+++ cmake/macros/TargetGlm.cmake
@@ -1,17 +1,14 @@
 # 
 #  Copyright 2015 High Fidelity, Inc.
+#  Copyright 2025 Overte e.V.
 #  Created by Bradley Austin Davis on 2015/10/10
 #
 #  Distributed under the Apache License, Version 2.0.
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 macro(TARGET_GLM)
-    if (ANDROID)
-        target_include_directories(${TARGET_NAME} PUBLIC "${VCPKG_INSTALL_ROOT}/include")
-    else()
-        find_package(glm CONFIG REQUIRED)
-        target_link_libraries(${TARGET_NAME} glm::glm)
-    endif()
+    find_package(glm CONFIG REQUIRED)
+    target_link_libraries(${TARGET_NAME} glm::glm)
     target_compile_definitions(${TARGET_NAME} PUBLIC GLM_FORCE_RADIANS)
     target_compile_definitions(${TARGET_NAME} PUBLIC GLM_ENABLE_EXPERIMENTAL)
     target_compile_definitions(${TARGET_NAME} PUBLIC GLM_FORCE_CTOR_INIT)
