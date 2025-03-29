779c170758ec287ff7347af03d915cc35b82e0d5
Julian Groß <julian.g@posteo.de>
2025-03-29 21:31:29 +0100

    Simplify Target macros.

--- cmake/macros/TargetBullet.cmake
+++ cmake/macros/TargetBullet.cmake
@@ -1,23 +1,13 @@
-# 
+#
 #  Copyright 2015 High Fidelity, Inc.
+#  Copyright 2025 Overte e.V.
 #  Created by Bradley Austin Davis on 2015/10/10
 #
 #  Distributed under the Apache License, Version 2.0.
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-# 
+#
 macro(TARGET_BULLET)
-    if (ANDROID)
-        set(INSTALL_DIR ${HIFI_ANDROID_PRECOMPILED}/bullet)
-        set(BULLET_INCLUDE_DIRS "${INSTALL_DIR}/include/bullet" CACHE STRING INTERNAL)
-
-        set(LIB_DIR ${INSTALL_DIR}/lib)
-        list(APPEND BULLET_LIBRARIES ${LIB_DIR}/libBulletDynamics.a)
-        list(APPEND BULLET_LIBRARIES ${LIB_DIR}/libBulletCollision.a)
-        list(APPEND BULLET_LIBRARIES ${LIB_DIR}/libLinearMath.a)
-        list(APPEND BULLET_LIBRARIES ${LIB_DIR}/libBulletSoftBody.a)
-    else()
-        find_package(Bullet REQUIRED)
-   endif()
+    find_package(Bullet REQUIRED)
     target_link_libraries(${TARGET_NAME} Bullet::Bullet)
 endmacro()
 
