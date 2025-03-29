cf5b7f31dbc0b248fde51771c8d7eedd58bf0f2b
Julian Groß <julian.g@posteo.de>
2025-03-29 21:31:29 +0100

    Simplify Target macros.

--- cmake/macros/TargetOpenSSL.cmake
+++ cmake/macros/TargetOpenSSL.cmake
@@ -1,19 +1,13 @@
 #
 #  Copyright 2015 High Fidelity, Inc.
+#  Copyright 2025 Overte e.V.
 #  Created by Bradley Austin Davis on 2015/10/10
 #
 #  Distributed under the Apache License, Version 2.0.
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 #
 macro(TARGET_OPENSSL)
-    if (ANDROID)
-        set(OPENSSL_INSTALL_DIR ${HIFI_ANDROID_PRECOMPILED}/openssl)
-        set(OPENSSL_INCLUDE_DIR "${OPENSSL_INSTALL_DIR}/include" CACHE STRING INTERNAL)
-        set(OPENSSL_LIBRARIES "${OPENSSL_INSTALL_DIR}/lib/libcrypto.a;${OPENSSL_INSTALL_DIR}/lib/libssl.a" CACHE STRING INTERNAL)
-    else()
-        find_package(OpenSSL 1.1.0 REQUIRED)
-    endif()
-
+    find_package(OpenSSL 1.1.0 REQUIRED)
     include_directories(SYSTEM "${OPENSSL_INCLUDE_DIR}")
     target_link_libraries(${TARGET_NAME} ${OPENSSL_LIBRARIES})
 endmacro()
--- cmake/macros/TargetTBB.cmake
+++ cmake/macros/TargetTBB.cmake
@@ -1,25 +1,12 @@
 #
 #  Copyright 2015 High Fidelity, Inc.
-#  Copyright 2024 Overte e.V.
+#  Copyright 2024-2025 Overte e.V.
 #  Created by Bradley Austin Davis on 2015/10/10
 #
 #  Distributed under the Apache License, Version 2.0.
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 #
 macro(TARGET_TBB)
-
-if (ANDROID)
-    set(TBB_INSTALL_DIR ${HIFI_ANDROID_PRECOMPILED}/tbb)
-    set(TBB_INCLUDE_DIRS ${TBB_INSTALL_DIR}/include CACHE FILEPATH "TBB includes location")
-    set(TBB_LIBRARY ${TBB_INSTALL_DIR}/lib/release/libtbb.so CACHE FILEPATH "TBB library location")
-    set(TBB_MALLOC_LIBRARY ${TBB_INSTALL_DIR}/lib/release/libtbbmalloc.so CACHE FILEPATH "TBB malloc library location")
-    set(TBB_LIBRARIES ${TBB_LIBRARY} ${TBB_MALLOC_LIBRARY})
-    target_include_directories(${TARGET_NAME} SYSTEM PUBLIC ${TBB_INCLUDE_DIRS})
-    target_link_libraries(${TARGET_NAME} ${TBB_LIBRARIES})
-else()
-    # using Conan for TBB
     find_package(TBB CONFIG REQUIRED)
     target_link_libraries(${TARGET_NAME} TBB::tbb)
-endif()
-
 endmacro()
