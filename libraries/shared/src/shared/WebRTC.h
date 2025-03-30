370fa97fce603332c3ee80db1fddc4f04d049401
Julian Groß <julian.g@posteo.de>
2025-03-30 14:46:10 +0200

    Disable WebRTC on Windows. It seems to conflict with parts of the Windows SDK.

--- libraries/shared/src/shared/WebRTC.h
+++ libraries/shared/src/shared/WebRTC.h
@@ -4,7 +4,7 @@
 //
 //  Copyright 2019 High Fidelity, Inc.
 //  Copyright 2021 Vircadia contributors.
-//  Copyright 2023 Overte e.V.
+//  Copyright 2023-2025 Overte e.V.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
@@ -26,13 +26,14 @@
 #  define WEBRTC_POSIX 1
 #  define WEBRTC_LEGACY 1
 #elif defined(Q_OS_WIN)
-#  define WEBRTC_AUDIO 1
-#  define WEBRTC_DATA_CHANNELS 1
-#  define WEBRTC_WIN 1
-#  define NOMINMAX 1
-#ifndef WIN32_LEAN_AND_MEAN
-#  define WIN32_LEAN_AND_MEAN 1
-#endif
+// Our WebRTC binary package seem to be broken on newer Windows SDKs.
+// #  define WEBRTC_AUDIO 1
+// #  define WEBRTC_DATA_CHANNELS 1
+// #  define WEBRTC_WIN 1
+// #  define NOMINMAX 1
+// #ifndef WIN32_LEAN_AND_MEAN
+// #  define WIN32_LEAN_AND_MEAN 1
+// #endif
 #elif defined(Q_OS_ANDROID)
 // I don't yet have a working libwebrtc for android
 // #  define WEBRTC_AUDIO 1
