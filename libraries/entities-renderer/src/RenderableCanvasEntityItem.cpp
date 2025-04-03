70fe3e84286c14bf95359ffcd91a0cf88223b160
Ada <ada+github@thingvellir.net>
2025-04-03 21:00:41 +1000

    Get textures working again, broken attempt at self-rendering

--- libraries/entities-renderer/src/RenderableCanvasEntityItem.cpp
+++ libraries/entities-renderer/src/RenderableCanvasEntityItem.cpp
@@ -8,13 +8,91 @@
 
 #include "RenderableCanvasEntityItem.h"
 
+#include <DependencyManager.h>
+#include <GeometryCache.h>
+#include <graphics/ShaderConstants.h>
+
+#include "RenderPipelines.h"
+
 using namespace render;
 using namespace render::entities;
 
-CanvasEntityRenderer::CanvasEntityRenderer(const EntityItemPointer& entity) : Parent(entity) { }
+CanvasEntityRenderer::CanvasEntityRenderer(const EntityItemPointer& entity) : Parent(entity) {
+    _geometryId = DependencyManager::get<GeometryCache>()->allocateID();
+    _material->setCullFaceMode(graphics::MaterialKey::CullFaceMode::CULL_NONE);
+    addMaterial(graphics::MaterialLayer(_material, 0), "0");
+    updateMaterials(true);
+}
 
-CanvasEntityRenderer::~CanvasEntityRenderer() { }
+CanvasEntityRenderer::~CanvasEntityRenderer() {
+    auto geometryCache = DependencyManager::get<GeometryCache>();
+    if (geometryCache) {
+        geometryCache->releaseID(_geometryId);
+    }
+}
 
 void CanvasEntityRenderer::doRenderUpdateAsynchronousTyped(const TypedEntityPointer& entity) {
     _texture = entity->getTexture();
+}
+
+void CanvasEntityRenderer::doRenderUpdateSynchronousTyped(const ScenePointer& scene, Transaction& transaction, const TypedEntityPointer& entity) {
+    void* key = (void*)this;
+    AbstractViewStateInterface::instance()->pushPostUpdateLambda(key, [this, entity] {
+        withWriteLock([&] {
+            _renderTransform = getModelTransform();
+            _renderTransform.postScale(entity->getScaledDimensions());
+        });
+    });
+}
+
+void CanvasEntityRenderer::doRender(RenderArgs* args) {
+    PerformanceTimer perfTimer("RenderableCanvasEntityItem::render");
+    Q_ASSERT(args->_batch);
+
+    graphics::MultiMaterial materials;
+    {
+        std::lock_guard<std::mutex> lock(_materialsLock);
+        materials = _materials["0"];
+    }
+
+    if (!_texture) {
+        return;
+    }
+
+    Transform transform;
+
+    gpu::Batch* batch = args->_batch;
+
+    bool usePrimaryFrustum = args->_renderMode == RenderArgs::RenderMode::SHADOW_RENDER_MODE || args->_mirrorDepth > 0;
+    transform.setRotation(BillboardModeHelpers::getBillboardRotation(transform.getTranslation(), transform.getRotation(), _billboardMode,
+        usePrimaryFrustum ? BillboardModeHelpers::getPrimaryViewFrustumPosition() : args->getViewFrustum().getPosition()));
+
+    batch->setModelTransform(transform, _prevRenderTransform);
+    if (args->_renderMode == Args::RenderMode::DEFAULT_RENDER_MODE || args->_renderMode == Args::RenderMode::MIRROR_RENDER_MODE) {
+        _prevRenderTransform = transform;
+    }
+
+    Pipeline pipelineType = getPipelineType(materials);
+    if (pipelineType == Pipeline::PROCEDURAL) {
+        auto procedural = std::static_pointer_cast<graphics::ProceduralMaterial>(materials.top().material);
+        procedural->prepare(*batch, transform.getTranslation(), transform.getScale(), transform.getRotation(), _created, ProceduralProgramKey(true));
+    } else if (pipelineType == Pipeline::SIMPLE) {
+        batch->setResourceTexture(0, _texture);
+    } else if (pipelineType == Pipeline::MATERIAL) {
+        if (RenderPipelines::bindMaterials(materials, *batch, args->_renderMode, args->_enableTexturing)) {
+            args->_details._materialSwitches++;
+        }
+    }
+
+    DependencyManager::get<GeometryCache>()->renderQuad(
+        *batch, glm::vec2(-0.5f), glm::vec2(0.5f), glm::vec2(0.0f), glm::vec2(1.0f),
+        glm::vec4(1.0f), _geometryId
+    );
+
+    if (pipelineType == Pipeline::SIMPLE) {
+        // we have to reset this to white for other simple shapes
+        batch->setResourceTexture(graphics::slot::texture::Texture::MaterialAlbedo, DependencyManager::get<TextureCache>()->getWhiteTexture());
+    }
+
+    qCritical("Canvas rendered");
 }
