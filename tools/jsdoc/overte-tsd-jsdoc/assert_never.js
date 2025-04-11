e32388b9379ea2298e16c0cb49171b22e9c681d8
UnTamedStArBlOb <qeu-b-458@zqueen.org>
2025-04-11 14:16:36 -0400

    reverting adding the ts template to repo

--- tools/jsdoc/overte-tsd-jsdoc/assert_never.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function assertNever(x) {
-    throw new Error("Unexpected object: " + JSON.stringify(x));
-}
-exports.assertNever = assertNever;
-//# sourceMappingURL=assert_never.js.map
