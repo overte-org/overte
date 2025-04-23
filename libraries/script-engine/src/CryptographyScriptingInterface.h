079faf852f7ee48e9f3e7ed2f74dcb4a7b3659f6
armored-dragon <publicmail@armoreddragon.com>
2025-04-23 07:02:02 -0500

    RSA key pair generation.

--- libraries/script-engine/src/CryptographyScriptingInterface.h
+++ libraries/script-engine/src/CryptographyScriptingInterface.h
@@ -3,8 +3,18 @@
 #include <QtCore/QObject>
 #include <QUuid>
 
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/provider.h>
+
 class CryptographyScriptingInterface : public QObject {
     Q_OBJECT
 public:
+    CryptographyScriptingInterface();
+    ~CryptographyScriptingInterface();
+
     Q_INVOKABLE static QUuid randomUUID();
+    Q_INVOKABLE QVariant generateRSAKeypair(const int bits = 2048);
+private: 
+    OSSL_LIB_CTX* libctx; // OpenSSL library context
 };
