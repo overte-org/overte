acf44be630ade78c3c9577fc82277f5e886749b2
armored-dragon <publicmail@armoreddragon.com>
2025-05-01 09:30:12 -0500

    Fix double app in debugger.

--- scripts/developer/debugging/cryptography/modules/rsa.js
+++ scripts/developer/debugging/cryptography/modules/rsa.js
@@ -1,6 +1,4 @@
 
-Script.require('../cryptographyDebugger.js');
-
 const generateRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 function runRSATest(bitLength) {
