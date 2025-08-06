147c59acbed34393cbe8be81cef7be75d6308bd4
Julian Groß <julian.g@posteo.de>
2025-08-06 21:41:53 +0200

    Force patched Qt version.

--- conanfile.py
+++ conanfile.py
@@ -93,7 +93,7 @@ class Overte(ConanFile):
         elif self.options.qt_source == "aqt":
             self.requires("qt/5.15.2@overte/aqt", force=True)
         else:
-            self.requires("qt/5.15.17-2025.06.07@overte/stable", force=True)
+            self.requires("qt/5.15.17-2025.06.07@overte/stable#e50c7cc7fafebef577edc7987d88fba2", force=True)
 
         if self.settings.os == "Windows":
             self.requires("neuron/12.2@overte/prebuild")
