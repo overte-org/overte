2cec5c79b315023632a724d2b40201935e56be83
Julian Groß <julian.g@posteo.de>
2025-08-16 09:14:18 +0200

    Try crosscompile workaround.

--- conanfile.py
+++ conanfile.py
@@ -56,7 +56,7 @@ class Overte(ConanFile):
         self.requires("glad/0.1.36")
         self.requires("gli/cci.20210515")
         self.requires("liblo/0.30@overte/stable")
-        self.requires("libnode/22.17.1@overte/experimental#5708b0d54d7aa0415962e11af2603b74")
+        self.requires("libnode/22.17.1@overte/experimental#8657a69a9c15a1c2704616282e7c3456")
         self.requires("nlohmann_json/3.11.2")
         self.requires("nvidia-texture-tools/2023.01@overte/stable")
         self.requires("onetbb/2021.10.0")
