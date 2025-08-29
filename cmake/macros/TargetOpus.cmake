831654d6f50ac5cd6e90241998332d4b292546d3
RTUnreal <unreal@rtinf.net>
2025-08-29 20:22:34 +0200

    add compilation with system libraries (focused on nixos)
    Co-authored-by: Julian Groß  <julian.g@posteo.de>

--- cmake/macros/TargetOpus.cmake
+++ cmake/macros/TargetOpus.cmake
@@ -6,6 +6,13 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 #
 macro(TARGET_opus)
-    find_package(Opus REQUIRED)
-    target_link_libraries(${TARGET_NAME} Opus::opus)
+    if(OVERTE_USE_SYSTEM_LIBS)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(Opus REQUIRED opus)
+        target_include_directories(${TARGET_NAME} SYSTEM PRIVATE ${Opus_INCLUDE_DIRS})
+        target_link_libraries(${TARGET_NAME} ${Opus_LINK_LIBRARIES})
+    else()
+        find_package(Opus REQUIRED)
+        target_link_libraries(${TARGET_NAME} Opus::opus)
+    endif()
 endmacro()
