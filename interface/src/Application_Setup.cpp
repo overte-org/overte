d62bfa92f4b9ed520889b41f212a21cd358dd346
Karol Suprynowicz <ksuprynowicz@post.pl>
2025-09-30 23:47:43 +0200

    Re-enable QML rendering

--- interface/src/Application_Setup.cpp
+++ interface/src/Application_Setup.cpp
@@ -762,7 +762,8 @@ void Application::initialize(const QCommandLineParser &parser) {
         << NodeType::EntityServer << NodeType::AssetServer << NodeType::MessagesMixer << NodeType::EntityScriptServer);
 
     // setDefaultFormat has no effect after the platform window has been created, so call it here.
-    QSurfaceFormat::setDefaultFormat(getDefaultOpenGLSurfaceFormat());
+    // QT6TODO: I was getting a warning when it was here, so I moved it to the beginning.
+    //QSurfaceFormat::setDefaultFormat(getDefaultOpenGLSurfaceFormat());
 
 #ifdef USE_GL
     _primaryWidget = new GLCanvas();
