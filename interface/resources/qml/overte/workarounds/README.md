6ad437fc0d5d12a7a7e34603d584dc4b2e170569
Ada <ada+github@thingvellir.net>
2025-10-27 22:12:57 +1000

    Functional file picker

--- interface/resources/qml/overte/workarounds/README.md
+++
@@ -1,4 +0,0 @@
-These are to work around the current desktop system, which requires
-components to be wrapped in a Window to have a draggable border and titlebar.
-
-This isn't required on the tablet because it doesn't support multiple windows at once.
--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -153,7 +153,7 @@ Menu::Menu() {
     auto action = addActionToQMenuAndActionHash(editMenu, MenuOption::RunningScripts, static_cast<int>(Qt::CTRL) | static_cast<int>(Qt::Key_J));
     connect(action, &QAction::triggered, [] {
         if (!qApp->getLoginDialogPoppedUp()) {
-            static const QUrl widgetUrl("overte/workarounds/RunningScripts_Window.qml");
+            static const QUrl widgetUrl("overte/compat/RunningScripts_Window.qml");
             static const QUrl tabletUrl("overte/dialogs/RunningScriptsDialog.qml");
             static const QString name("RunningScripts");
             qApp->showDialog(widgetUrl, tabletUrl, name);
