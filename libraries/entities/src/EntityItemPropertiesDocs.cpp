a8083db84bc2ac95ff7f8ce4e5eaea875fbdf71e
Zedadias Wick <zedwick@gmail.com>
2026-01-02 22:21:03 +0000

    Changed image entity example to the same url as is default in the Create app

--- libraries/entities/src/EntityItemPropertiesDocs.cpp
+++ libraries/entities/src/EntityItemPropertiesDocs.cpp
@@ -998,8 +998,8 @@
  * var image = Entities.addEntity({
  *     type: "Image",
  *     position: Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 0, z: -2 })),
- *     dimensions: { x: 1.0, y: 0.26, z: 0.01 },
- *     imageURL: "https://apidocs.overte.org/images/brand-logo-512.png",
+ *     dimensions: { x: 1.0, y: 0.5, z: 0.01 },
+ *     imageURL: "https://content.overte.org/Bazaar/Assets/Textures/Defaults/Interface/default_image.jpg",
  *     billboardMode: "yaw",
  *     lifetime: 300  // Delete after 5 minutes.
  * });
