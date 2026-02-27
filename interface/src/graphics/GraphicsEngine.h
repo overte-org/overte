e245fa9a66f90209ca424c947946175421d68d4c
ksuprynowicz <ksuprynowicz@post.pl>
2026-02-27 18:31:02 +0100

    Documenting GraphicsEngine and RenderEventHandler

--- interface/src/graphics/GraphicsEngine.h
+++ interface/src/graphics/GraphicsEngine.h
@@ -112,26 +112,25 @@ public:
     bool shouldPaint() const;
 
     /**
+     * Used by `Application::onPresent` to check if a new Frame object creation event can be posted.
+     * Internally it changes state of `_pendingRenderingEvent` atomic_bool.
      *
-     * @return
+     * @return `true` if previous Frame object creation event has finished and new one can be posted.
      */
     bool checkPendingRenderEvent();
 
     /**
-     *
-     * @return
+     * @return Number of Frame objects generated this far. Does not have to be equal to number of frames actually rendered.
      */
     size_t getRenderFrameCount() const { return _renderFrameCount; }
 
     /**
-     *
-     * @return
+     * @return Framerate of the "Render" loop. Render loop does not actually render, it only generates frames to be rendered on a separate thread.
      */
     float getRenderLoopRate() const { return _renderLoopCounter.rate(); }
 
-    // Feed Graphics Engine with new frame configuration
     /**
-     *
+     * Give Graphics Engine new frame configuration.
      * @param editor
      */
     void editRenderArgs(RenderArgsEditor editor);
@@ -139,79 +138,81 @@ public:
 private:
     // Thread specific calls
     /**
-     *
+     * Creates a new frame object with all rendering operations and submits it to the display plugin.
+     * Does not actually render frame.
      */
     void render_performFrame();
 
     /**
-     *
-     * @param renderArgs
+     * Called by `render_performFrame`. Generates `Frame` object to be rendered later on the Present thread.
+     * @param renderArgs Configuration for this frame.
      */
     void render_runRenderFrame(RenderArgs* renderArgs);
 
 protected:
     /**
-     *
+     * Mutex used to protect frame configuration.
      */
     mutable QRecursiveMutex _renderArgsMutex;
 
     /**
-     *
+     * Frame configuration. Updated from the main thread and used by the "Render" thread, which generates `Frame` objects but
+     * does not render them.
      */
     AppRenderArgs _appRenderArgs;
 
     /**
-     *
+     * Used for measuring rate at which frame objects are generated.
      */
     RateCounter<500> _renderLoopCounter;
 
     /**
-     *
+     * Counts number of frame objects that were created.
      */
     uint32_t _renderFrameCount{ 0 };
 
     /**
-     *
+     * DOCTODO
      */
     render::ScenePointer _renderScene{ new render::Scene(glm::vec3(-0.5f * (float)TREE_SCALE), (float)TREE_SCALE) };
 
     /**
-     *
+     * `RenderEngine` object contains all the tasks needed to generate a Frame object that can later be submitted to
+     * display plugin to be rendered.
      */
     render::EnginePointer _renderEngine{ new render::RenderEngine() };
 
     /**
-     *
+     * Object that manages rendering backend and GPU statistics.
      */
     gpu::ContextPointer _gpuContext; // initialized during window creation
 
     /**
-     *
+     * `RenderEventHandler` responsible for creating Frame objects and submitting them to display plugin, where later
+     * they get rendered on Present thread.
      */
     QObject* _renderEventHandler{ nullptr };
 
-    /**
-     *
-     */
     friend class RenderEventHandler;
 
     /**
-     *
+     * Used for "FrameTimings" scripting API.
      */
     FrameTimingsScriptingInterface _frameTimingsScriptingInterface;
 
     /**
-     *
+     * A simple skybox shown before the shaders are compiled.
      */
     std::shared_ptr<ProceduralSkybox> _splashScreen { std::make_shared<ProceduralSkybox>() };
 
     /**
-     *
+     * Splash screen texture. Shown as a skybox before the shaders load and compile.
      */
     NetworkTexturePointer _texture;
 #ifndef Q_OS_ANDROID
     /**
-     *
+     * Changes to true when the shaders are loaded and compiled.
+     * Used to show the splash screen skybox.
      */
     std::atomic<bool> _programsCompiled { false };
 #else
