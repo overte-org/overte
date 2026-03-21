58129c469a5f2d161504b0dcb389c71c8052f121
Ada <ada+github@thingvellir.net>
2026-03-22 00:10:46 +1000

    Flat depth-biased Text entities instead of physical offset

--- libraries/entities-renderer/src/RenderableTextEntityItem.cpp
+++ libraries/entities-renderer/src/RenderableTextEntityItem.cpp
@@ -122,6 +122,9 @@ ItemKey TextEntityRenderer::getKey() {
 }
 
 ShapeKey TextEntityRenderer::getShapeKey() {
+    // FIXME: ShapeKey::Builder has no way of setting a specific
+    // depth offset like we'd need to offset both the background
+    // and payload in order
     auto builder = render::ShapeKey::Builder().withDepthBias();
     updateShapeKeyBuilderFromMaterials(builder);
     return builder.build();
@@ -294,7 +297,10 @@ ShapeKey entities::TextPayload::getShapeKey() const {
         if (renderable) {
             auto textRenderable = std::static_pointer_cast<TextEntityRenderer>(renderable);
 
-            auto builder = render::ShapeKey::Builder().withOwnPipeline();
+            // FIXME: ShapeKey::Builder has no way of setting a specific
+            // depth offset like we'd need to offset both the background
+            // and payload in order
+            auto builder = render::ShapeKey::Builder().withOwnPipeline().withDepthBias();
             if (textRenderable->isTextTransparent()) {
                 builder.withTranslucent();
             }
@@ -382,7 +388,7 @@ void entities::TextPayload::render(RenderArgs* args) {
     if (fontHeight > 0.0f) {
         scale = textRenderable->_lineHeight / fontHeight;
     }
-    transform.postTranslate(glm::vec3(-0.5, 0.5, 1.0f + EPSILON / dimensions.z));
+    transform.postTranslate(glm::vec3(-0.5f, 0.5f, 0.0f));
     transform.setScale(scale);
     batch.setModelTransform(transform, _prevRenderTransform);
     if (args->_renderMode == Args::RenderMode::DEFAULT_RENDER_MODE || args->_renderMode == Args::RenderMode::MIRROR_RENDER_MODE) {
