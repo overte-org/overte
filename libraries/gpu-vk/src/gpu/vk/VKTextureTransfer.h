6949ed21e5c42df059e1a1a89afff6d343984098
Julian Groß <julian.g@posteo.de>
2026-06-09 17:45:43 +0200

    Fix copyright headers. The "Created by" section at the top looks like it was automatically created by CLion. The rest was copied from another file and not updated. To quote the Apache 2.0 license: "Unless You explicitly state otherwise, any Contribution intentionally submitted for inclusion in the Work by You to the Licensor shall be under the terms and conditions of this License, without any additional terms or conditions."

--- libraries/gpu-vk/src/gpu/vk/VKTextureTransfer.h
+++ libraries/gpu-vk/src/gpu/vk/VKTextureTransfer.h
@@ -1,10 +1,6 @@
 //
-// Created by ksuprynowicz on 18.05.2026.
-//
-
-//
-//  Created by Bradley Austin Davis on 2016/05/15
-//  Copyright 2013-2016 High Fidelity, Inc.
+//  Created by Dr. Karol Suprynowicz on 18.05.2026.
+//  Copyright 2026 Overte e.V.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
