44d6c872f4f63cb1b732c22c2a9d075dfef5da98
Zedadias Wick <zedwick@gmail.com>
2026-06-14 15:13:39 +0100

    Create: draggableNumber.js > use strict

--- scripts/system/create/entityProperties/html/js/draggableNumber.js
+++ scripts/system/create/entityProperties/html/js/draggableNumber.js
@@ -5,6 +5,7 @@
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+"use strict";
 
 const DELTA_X_FOCUS_THRESHOLD = 1;
 const ENTER_KEY = 13;
